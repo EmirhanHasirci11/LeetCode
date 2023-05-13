7de69ed8489e1d661d3ff8a14247d4985fd413e1
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-05-13 23:36:08 +0300

    Sqrt(x)

---
+++ Sqrt(x)/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace Sqrt_x_
+{
+    internal class Solution
+    {       
+        //Newton Method
+        //iteration count must be dynamic but i cant find it how to do.
+        public static int MySqrt(int x)
+        {
+            double root = 1;
+            for (int i = 0; i <= 20; i++)
+            {
+                root = (x / root + root) / 2;
+            }            
+            return Convert.ToInt32(root.ToString().Split(".")[0]);
+        }
+    }
+}
