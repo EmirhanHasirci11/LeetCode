fbbd37bfb4ab1aae736e5a032d60123f1ad38d63
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-05-13 22:27:06 +0300

    PalindromeNumber

---
+++ PalindromeNumber/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace PalindromeNumber
+{
+    internal class Solution
+    {        
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+            string pal = x.ToString();
+            int left = 0;
+            int right = x.ToString().Length-1;
+            while (left < right)
+            {
+                if (pal[left++] != pal[right--])
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        //////Solution2//////
+        //////With built in methods/////
+        public bool IsPalindrome2(int x)
+        {
+            Char[] y =x.ToString().ToCharArray();
+            Array.Reverse(y);
+            return x.ToString() == new string(y);
+        }
+    }
+}
