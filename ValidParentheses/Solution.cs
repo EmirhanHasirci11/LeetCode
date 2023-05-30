04d0da1c82b3f252d9200c35a9e54d207b15a610
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-05-30 11:22:33 +0300

    ValidParentheses

---
+++ ValidParentheses/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace ValidParentheses
+{
+    internal class Solution
+    {       
+        public  bool IsValid(string s)
+        {
+            Stack<char> stack = new();
+            if (s.Length < 2)
+                return false;
+            if (s.StartsWith(')') || s.StartsWith(']') || s.StartsWith('}'))
+                return false;
+            foreach (char x in s)
+            {
+
+                if (x == '(' || x == '{' || x == '[')
+                    stack.Push(x);
+                else if (stack.Count != 0)
+                {
+                    if ((x == ']' && stack.Peek() == '[' || x == '}' && stack.Peek() == '{') || x == ')' && stack.Peek() == '(')
+                        stack.Pop();
+                    else
+                        return false;
+                }
+                else
+                    return false;
+
+            }
+            if (!stack.Any())
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
