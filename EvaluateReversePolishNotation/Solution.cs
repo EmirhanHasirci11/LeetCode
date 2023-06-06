8368af78684a2a9e7fa1ad1409a154e8a5a83d68
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-07 00:06:28 +0300

    EvaluateReversePolishNotation

---
+++ EvaluateReversePolishNotation/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace EvaluateReversePolishNotation
+{
+    public class Solution
+    {
+        private static readonly Dictionary<string, Func<int, int, int>> s_Funcs = new() {
+      { "+", (right, left) => left + right},
+      { "-", (right, left) => left - right },
+      { "*", (right, left) => left * right },
+      { "/", (right, left) => left / right },
+    };
+
+        public int EvalRPN(string[] tokens)
+        {
+            Stack<int> data = new();
+            foreach (string token in tokens)
+                if (int.TryParse(token, out int value))
+                    data.Push(value);
+                else
+                    data.Push(s_Funcs[token](data.Pop(), data.Pop()));
+            return data.Pop();
+        }
+    }
+}
