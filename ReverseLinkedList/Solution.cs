e54b3b79da0d9a2e0db16664789e58faa05dd4a2
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-08 13:54:15 +0300

    ReverseLinkedList

---
+++ ReverseLinkedList/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace ReverseLinkedList
+{
+    internal class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode next;
+            while (head != null)
+            {
+                next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+            head = prev;
+            return head;
+        }
+    }
+}
