5dc17b7a11da7f664dae4ad6ac448c43cc37c0fa
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-09 04:48:59 +0300

    RemoveDuplicatesfromSortedArray

---
+++ RemoveDuplicatesfromSortedArray/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace RemoveDuplicatesfromSortedArray
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int control = 1;
+            foreach (int number in nums)
+            {
+                if (nums[control - 1] != number)
+                    nums[control++] = number;
+
+            }
+            return control;
+        }
+    }
+}
