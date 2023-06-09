640d0ad573a1bf24fe277fba025c83872d1acf76
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-09 15:25:20 +0300

    ReverseLinkedListII

---
+++ ReverseLinkedListII/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace ReverseLinkedListII
+{
+    public class Solution
+    {
+        
+        public  ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            ListNode current = new(0,head);
+            ListNode prev= current; 
+            for(int i=0;i<left-1;i++)
+                prev=prev.next;
+            ListNode startPos = prev.next;
+            ListNode next =startPos.next;
+            for(int i = 0; i < right - left; i++)
+            {
+                startPos.next = next.next;
+                next.next= prev.next;
+                prev.next = next;
+                next=startPos.next;
+            }
+            return current.next;
+        }
+
+    }
+}
