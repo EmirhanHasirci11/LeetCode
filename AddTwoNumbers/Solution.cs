ec3696520464f43f43e4f9c2a37e2c339b61ad8f
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-10 13:05:28 +0300

    AddTwoNumbers

---
+++ AddTwoNumbers/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace AddTwoNumbers
+{
+    internal class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode head = new();
+            ListNode current = head;
+            int carry = 0;
+            while (l1 != null || l2 != null || carry == 1)
+            {
+                int sum = 0;
+                if (l1 != null)
+                {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+                sum += carry;
+                carry = sum / 10;
+                ListNode currentSum = new(sum % 10);
+                current.next = currentSum;
+                current = current.next;
+            }
+            return head.next;
+
+        }
+    }
+}
