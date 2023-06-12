eb1dd3a3a98195f9ddc8b5a6b4cc80231690c422
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-12 10:16:31 +0300

    MergeTwoSortedLists

---
+++ MergeTwoSortedLists/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace MergeTwoSortedLists
+{   
+    internal class Solution
+    {     
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode mergedList = new();
+            ListNode head = mergedList;
+            while (list1 is not null && list2 is not null )
+            {
+                if (list1.val < list2.val)
+                {
+                    mergedList.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    mergedList.next = list2;
+                    list2 = list2.next;
+                }
+                
+                mergedList=mergedList.next;
+            }
+
+            mergedList.next = list1 ?? list2;
+
+            return head.next;
+        }
+    }
+}
