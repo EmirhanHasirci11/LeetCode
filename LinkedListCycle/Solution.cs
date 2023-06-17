55b93081d097425804e225c47eaabcfe3f971a9a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-18 01:07:47 +0300

    LinkedListCycle

---
+++ LinkedListCycle/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LinkedListCycle
+{
+    internal class Solution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            ListNode slowest = head;
+            ListNode fastest = head;
+            while (fastest != null && fastest.next != null)
+            {
+                slowest = slowest.next;
+                fastest = fastest.next.next;
+                if (slowest == fastest)
+                    return true;
+
+            }
+            return false;
+        }
+    }
+}
