9900c9a7417a096a650d6d61ae7f94b41c2d3248
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-19 00:05:15 +0300

    NumberOfLinesToWriteString

---
+++ NumberOfLinesToWriteString/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace NumberOfLinesToWriteString
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            int[] widths = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 }; 
+            var s = "abcdefghijklmnopqrstuvwxyz";
+            var result=NumberOfLines(widths, s);
+            foreach( var res in result ) 
+            {
+                Console.WriteLine(res);
+            }
+
+        }
+        public static int[] NumberOfLines(int[] widths, string s)
+        {
+            int countOfLines = 0;
+            int totalWide = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                totalWide += widths[s[i] - 'a'];
+                if (totalWide > 100)
+                {
+                    totalWide = 0;
+                    countOfLines++;
+                    totalWide += widths[s[i] - 'a'];
+                }
+            }
+            return new int[] { ++countOfLines, totalWide };
+        }
+    }
+}
