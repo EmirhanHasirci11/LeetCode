aa79b17ccd385343a287ab25209518ca556e3981
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-20 19:01:56 +0300

    FizzBuzz

---
+++ FizzBuzz/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public IList<string> FizzBuzz(int n)
+        {
+            List<string> list = new();
+            for(int i = 1; i <= n; i++)
+            {
+                if (n % 15 == 0)
+                    list.Add("FizzBuzz");
+                else
+                    list.Add(i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i.ToString());
+            }
+            
+            return list;
+        }
+    }
+}
