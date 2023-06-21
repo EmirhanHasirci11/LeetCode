b324759d15dc7a799285bd860fd494a3626791ee
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-21 17:10:59 +0300

    Numberof1Bits

---
+++ Numberof1Bits/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace Numberof1Bits
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HammingWeight(00000000000000000000000000001011));
+        }
+        public static int HammingWeight(uint n)
+        {
+            uint result = 0;
+            while (n > 0)
+            {
+                result += n % 2;
+                n = n >> 1;
+            }
+            return (int)result;
+
+        }
+    }
+}
