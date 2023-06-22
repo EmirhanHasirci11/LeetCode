d850733d7db12734c20ea54776429dbfe5c3f2fe
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-22 16:51:02 +0300

    Create Solution.cs

---
+++ MajorityElement/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace MajorityElement
+{
+    internal class Program
+    {       
+        public static int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> map = new();
+            foreach(int num in nums)
+            {
+                if (map.ContainsKey(num))                
+                    map[num]++;                
+                else
+                    map[num] = 1;                                
+            }
+            foreach(var key in map.Keys)
+            {
+                if (map[key] > nums.Length / 2)
+                    return key;
+            }
+            return 0;
+        }
+    }
+}
