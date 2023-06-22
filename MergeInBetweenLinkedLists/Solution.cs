393b30089055cc8db830dea23edc2b9bf7ec3fec
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-22 15:40:03 +0300

    MergeInBetweenLinkedLists

---
+++ MergeInBetweenLinkedLists/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace MergeInBetweenLinkedLists
+{
+    internal class Solution
+    {
+        public ListNode MergeInBetween(ListNode list1, int a, int b, ListNode list2)
+        {
+            ListNode head = list1;
+            int temp = 0;
+            while (list1 != null && temp != a - 1)
+            {
+                list1 = list1.next;
+                temp++;
+            }
+            temp = a;
+            ListNode next = list1.next;
+            list1.next = list2;
+            while (next != null && temp != b + 1)
+            {
+                next = next.next;
+                temp++;
+            }
+            while (list2.next != null)
+                list2 = list2.next;
+
+            list2.next = next;
+            return head;
+        }
+
+    }
+}
