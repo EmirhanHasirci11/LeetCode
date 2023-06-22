78e5eebb97e3115e738f53c33003604d62567543
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-22 17:01:46 +0300

    FindtheHighestAltitude

---
+++ FindtheHighestAltitude/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace FindtheHighestAltitude
+{
+    internal class Program
+    {        
+        public static int LargestAltitude(int[] gain)
+        {
+            int maxAltitude = int.MinValue;
+            int sum = 0;
+            foreach(int i in gain)
+            {
+                sum+= i;
+                if(sum > maxAltitude)
+                {
+                    maxAltitude = sum;
+                }
+            }
+            return maxAltitude>0?maxAltitude:0;
+        }
+    }
+}
