3749d052c87bc5141b162816e56d7ca45ce0995f
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-23 12:16:16 +0300

    RemoveLinkedListElements

---
+++ RemoveLinkedListElements/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace RemoveLinkedListElements
+{
+    internal class Program
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            ListNode current = head;
+            ListNode prev = null;
+
+            while (current != null)
+            {
+                if (current.val == val)
+                {
+                    if (prev != null)
+                        prev.next = current.next;
+                    else
+                        head = head.next;
+                }
+                else
+                    prev = current;
+                current = current.next;
+            }
+            return head;
+        }
+    }
+}
