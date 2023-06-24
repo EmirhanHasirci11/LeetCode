324879ab61560715c00a5dd40d83a5ece743dd85
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-24 14:04:07 +0300

    AddDigits

---
+++ AddDigits/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace AddDigits
+{
+    internal class Solution
+    {        
+        public static int AddDigits(int num)
+        {
+            while (num % 10 != num)
+            {
+            int temp =0;
+                
+                while (num != 0)
+                {
+                    temp+=num % 10;
+                    num /= 10;
+                }
+                num = temp;
+            }
+            return num;
+        }
+    }
+}
