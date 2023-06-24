a1d966c15732e880f379d8dce185af0eb715e733
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-24 14:18:37 +0300

    CheckIfNandItsDoubleExist

---
+++ CheckIfNandItsDoubleExist/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace CheckIfNandItsDoubleExist
+{
+    internal class Solution
+    {
+        public bool CheckIfExist(int[] arr)
+        {
+            HashSet<int> hash = new();
+            foreach (int number in arr)
+            {
+                if (hash.Contains(number * 2) || (number % 2 == 0 && hash.Contains(number / 2)))
+                    return true;
+                hash.Add(number);
+            }
+            return false;
+        }
+    }
+}
