fd3506b461ac3e7a383c96b7e19f9b5306970081
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-24 14:02:43 +0300

    SmallestIndexWithEqualValue

---
+++ SmallestIndexWithEqualValue/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace SmallestIndexWithEqualValue
+{
+    internal class Solution
+    {
+        public int SmallestEqual(int[] nums)
+        {
+            int smallest = int.MaxValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 10 == nums[i])
+                {
+                    if (i < smallest)
+                        smallest = i;
+                }
+            }
+            return smallest < nums.Length ? smallest : -1;
+        }
+    }
+}
