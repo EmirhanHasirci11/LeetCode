ce1dfba68a28382b11f63ba455b30071ea53fc87
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-25 18:30:28 +0300

    CanPlaceFlowers

---
+++ CanPlaceFlowers/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace CanPlaceFlowers
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            int[] flowerbed = { 1, 0, 1, 0, 1, 0, 1 };
+            Console.WriteLine(CanPlaceFlowers(flowerbed, 1));
+        }
+        public static bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            for (int i = 0; i < flowerbed.Length && n > 0; i++)
+            {
+                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
+                    (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                {
+                    i++;
+                    n--;
+                }
+            }
+            return n == 0;
+        }
+    }
+}
