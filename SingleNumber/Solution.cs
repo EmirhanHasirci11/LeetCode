421cff63c6e40b08b06a088755e7955624107423
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-26 05:24:18 +0300

    SingleNumber

---
+++ SingleNumber/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace SingleNumber
+{
+    internal class Solution
+    {        
+        public int SingleNumber(int[] nums)
+        {
+            int sum = 0;            
+            foreach (int i in nums)            
+                sum ^=  i;
+            
+            return sum;
+        }
+    }
+}
