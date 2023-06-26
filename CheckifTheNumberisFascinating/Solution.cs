82aea0790e190b9629357b188b12d053baeb4a41
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-26 05:10:01 +0300

    CheckifTheNumberisFascinating

---
+++ CheckifTheNumberisFascinating/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace CheckifTheNumberisFascinating
+{
+    internal class Solution
+    {     
+        public bool IsFascinating(int n)
+        {
+            Dictionary<char, int> map = new();
+            string number = ""+n + n * 2 + n * 3;            
+            foreach(char c in number) 
+            {
+                if (c == '0') return false;
+                if(map.ContainsKey(c))
+                    map[c]++;
+                else
+                    map[c] = 1;
+            }            
+            for(int i='1';i<='9';i++) 
+            {
+                if (!map.ContainsKey((char)i)) return false;
+                else if(map[(char)i] != 1) return false;                                    
+            }
+            return true;
+        }
+    }
+}
