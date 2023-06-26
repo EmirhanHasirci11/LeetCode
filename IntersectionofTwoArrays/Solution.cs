a4e7cadc56408db396e0079ce71bc75cac7f6a55
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-26 05:41:13 +0300

    IntersectionofTwoArrays

---
+++ IntersectionofTwoArrays/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace IntersectionofTwoArrays
+{
+    internal class Solution
+    {      
+        public static int[] Intersection(int[] nums1, int[] nums2)
+        {
+            HashSet<int> first = new(nums1);
+            HashSet<int> second = new(nums2);
+            foreach (int n in first)
+                if (!second.Contains(n))
+                    first.Remove(n);
+            return first.ToArray();
+        }
+    }
+}
