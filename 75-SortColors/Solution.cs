9505ef2aa51769da2f78b7f22626ffc3f227f61f
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-26 06:16:16 +0300

    75-SortColors

---
+++ 75-SortColors/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace SortColors
+{
+    internal class Program
+    {
+        public void SortColors(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int key = nums[i];
+                int j = i - 1;
+                while (j >= 0 && nums[j] > key)
+                {
+                    nums[j + 1] = nums[j];
+                    j--;
+                }
+                nums[j + 1] = key;
+            }
+        }
+    }
+}
