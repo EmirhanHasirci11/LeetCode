d57ed006dae5c174364506dabea2e87cf79ee5c5
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-26 04:38:56 +0300

    FibonacciNumber

---
+++ FibonacciNumber/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace FibonacciNumber
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(4));
+        }
+        public static int Fib(int n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+            return Fib(n - 1) + Fib(n - 2);
+
+        }
+    }
+}
