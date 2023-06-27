f4c2cf7f33e8b715d9574b96c2ce355048c31cf3
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-28 00:57:50 +0300

    217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ContainsDuplicate
+{
+    internal class Solution
+    {
+       
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+            return nums.Length > set.Count;
+        }
+    }
+}
