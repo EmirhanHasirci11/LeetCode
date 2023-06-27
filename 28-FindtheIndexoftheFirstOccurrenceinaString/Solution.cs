57113c1caf3c0c1b582f609d90b7c5a07f8bd4da
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-27 16:02:45 +0300

    28-FindtheIndexoftheFirstOccurrenceinaString

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace FindtheIndexoftheFirstOccurrenceinaString
+{
+    internal class Solution
+    {              
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle == null) return -1;
+            int[] lps =new int [needle.Length];
+            int prevLPS = 0,j=0 ,i = 1;
+            while (i < needle.Length)
+            {
+                if (needle[i] == needle[prevLPS])
+                {
+                    lps[i] = prevLPS + 1;
+                    prevLPS++;
+                    i++;
+                }
+                else if (prevLPS == 0)
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+                else
+                    prevLPS = lps[prevLPS - 1];
+            }
+            i= 0;
+            while (i < haystack.Length)
+            {
+                if (haystack[i] == needle[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    if (j == 0)
+                        i++;
+                    else
+                        j = lps[j-1];
+                }
+                if (j == needle.Length)
+                    return i - needle.Length;
+            }
+
+            return -1;
+        }
+    }
+}
