223cd6a040a7cb09ac809824dea871e9387c8c13
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-28 15:47:21 +0300

    151-ReverseWordsinaString

---
+++ 151-ReverseWordsinaString/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace ReverseWordsinaString
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("the sky is blue"));
+        }
+        public static string ReverseWords(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] words = s.Split(' ');
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+
+                if (words[i].Length > 0)
+                {
+                    sb.Append(words[i]);
+                    sb.Append(' ');
+                }
+            }
+
+            if (sb.Length > 0)
+                sb.Remove(sb.Length - 1, 1);
+
+            return sb.ToString();
+        }
+
+    }
+}
