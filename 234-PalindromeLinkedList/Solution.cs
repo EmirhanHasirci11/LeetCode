426ab9bfa6a9b134227eec2b556ce0197af6b679
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-30 01:13:13 +0300

    234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace PalindromeLinkedList
+{
+    internal class Solution
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            StringBuilder stringBuilder = new();
+
+            while (head != null)
+            {
+                stringBuilder.Append(head.val);
+                head = head.next;
+            }
+            string x = stringBuilder.ToString();
+            return x.Reverse().SequenceEqual(x);
+        }
+    }
+}
