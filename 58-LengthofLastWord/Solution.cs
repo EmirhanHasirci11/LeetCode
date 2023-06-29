dec9251f99a16e1da70fb47a890ad0409873d658
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-29 03:05:44 +0300

    58-LengthofLastWord

---
+++ 58-LengthofLastWord/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LengthofLastWord
+{
+    internal class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            int count = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (Char.IsWhiteSpace(s[i]))
+                {
+                    if (count > 0)
+                        return count;
+                    continue;
+                }
+                count += 1;
+            }
+            return count;
+        }
+    }
+}
