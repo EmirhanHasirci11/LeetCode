1da956d52e331c55b47eb75ba36d1de8761b59f6
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-01 00:10:16 +0300

    382-LinkedListRandomNode

---
+++ 382-LinkedListRandomNode/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace LinkedListRandomNode
+{
+    internal class Solution
+    {
+        private Random random;
+        private ListNode head;
+
+        public Solution(ListNode head)
+        {
+            this.head = head;
+            this.random = new Random();
+        }
+
+        public int GetRandom()
+        {
+            ListNode current = head;
+            int count = 0;
+            while (current != null)
+            {
+                count++;
+                current = current.next;
+            }
+            current = head;
+            int temp = random.Next(count);
+            count = 0;
+            while (current != null && count != temp)
+            {
+
+                current = current.next;
+                count++;
+            }
+
+            return current.val;
+        }
+    }
+}
