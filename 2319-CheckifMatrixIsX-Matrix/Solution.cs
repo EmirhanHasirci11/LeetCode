90109e2d05a18ac1fc01a87fbc35114ef0f5b1f5
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-06-30 14:16:54 +0300

    2319-CheckifMatrixIsX-Matrix

---
+++ 2319-CheckifMatrixIsX-Matrix/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace CheckifMatrixIsX_Matrix
+{
+    internal class Program
+    {        
+        public bool CheckXMatrix(int[][] grid)
+        {
+            int n = grid.Length;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (row == col || row + col == n - 1)
+                    {
+                        if (grid[row][col] == 0) return false;
+                    }
+                    else
+                    {
+                        if (grid[row][col] != 0) return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
