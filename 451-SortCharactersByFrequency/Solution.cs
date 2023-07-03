c3ab51c58c86b46eb748b95dc991f9fbab74ebec
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-03 23:43:06 +0300

    451-SortCharactersByFrequency

---
+++ 451-SortCharactersByFrequency/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace SortCharactersByFrequency
+{
+    internal class Solution
+    {        
+        public string FrequencySort(string s)
+        {
+            Dictionary<char, int> map = new();
+            StringBuilder sb = new();
+
+            for(int i=0;i<s.Length;i++)
+            {
+                if (map.ContainsKey(s[i]))
+                    map[s[i]]++;
+                else
+                    map[s[i]] = 1;                
+            }
+            var keys = map.ToArray();
+
+            Array.Sort(keys,(first,second)=>second.Value-first.Value);
+
+            foreach(var key in keys)             
+                sb.Append(key.Key,key.Value);    
+            
+            return sb.ToString();
+
+                
+            
+        }
+    }
+}
