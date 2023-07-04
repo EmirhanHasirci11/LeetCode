df16910f6120cffd8b4c5bf16ad3db227e3310a9
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-04 17:38:55 +0300

    70-ClimbingStairs

---
+++ 70-ClimbingStairs/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace ClimbingStairs
+{
+    internal class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+
+            int first = 2;
+            int second = 1;
+            int current = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                current = first + second;
+                second = first;
+                first = current;
+            }
+            return current;
+        }
+    }
+}
