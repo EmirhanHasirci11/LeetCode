2998d52b865da69d2dd4dd0795835a6713e2eda9
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-05 09:14:34 +0300

    55-JumpGame

---
+++ 55-JumpGame/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace JumpGame
+{
+    internal class Solution
+    {
+        public bool CanJump(int[] nums)
+        {
+            int endcase = nums.Length - 1;
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                if (i + nums[i] >= endcase)
+                    endcase = i;
+            }
+            return endcase == 0;
+        }
+    }
+}
