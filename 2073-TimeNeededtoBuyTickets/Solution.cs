c47099d4e8ab263a22bd284782981ba5856e5072
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-06 18:07:49 +0300

    2073-TimeNeededtoBuyTickets

---
+++ 2073-TimeNeededtoBuyTickets/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace TimeNeededtoBuyTickets
+{
+    internal class Solution
+    {       
+        public int TimeRequiredToBuy(int[] tickets, int k)
+        {
+            int queue = 0;
+            while (tickets[k] != 0)
+            {
+                for(int i=0;i<tickets.Length;i++)
+                {
+                    if (tickets[i] == 0)
+                        continue;
+                    queue++;
+                    tickets[i]--;
+                    if (tickets[k] == 0)
+                        break;
+                }
+            }
+            return queue;
+
+
+        }
+    }
+
+}
