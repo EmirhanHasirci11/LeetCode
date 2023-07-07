c4c52970d2f3d9d65e5bf8634f5ddb815ed70545
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-07 20:16:45 +0300

    2119-ANumberAfteraDoubleReversal

---
+++ 2119-ANumberAfteraDoubleReversal/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace ANumberAfteraDoubleReversal
+{
+    internal class Program
+    {
+        public bool IsSameAfterReversals(int num)
+        {
+            return num == 0 || num % 10 != 0;
+        }
+    }
+}
