7bb1b3e77c714ca7711fba9c9adec67288151ef7
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-08 17:26:10 +0300

    389-FindtheDifference

---
+++ 389-FindtheDifference/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace FindtheDifference
+{
+    internal class Solution
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            int sum = 0;
+            foreach (char c in t)
+                sum += c;
+            foreach (char c in s)
+                sum -= c;
+            return (char)sum;
+        }
+    }
+}
