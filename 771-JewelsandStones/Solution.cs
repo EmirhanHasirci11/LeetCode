f36f4910b87a10493caec506345cd886c3cfc4d7
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-12 11:36:42 +0300

    771-JewelsandStones

---
+++ 771-JewelsandStones/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace JewelsandStones
+{
+    internal class Solution
+    {       
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            int countOfJewelStones = 0;
+            HashSet<char> charHash = new HashSet<char>(jewels);
+            foreach (char c in stones)
+            {
+                if(charHash.Contains(c))
+                    countOfJewelStones++;
+            }
+            return countOfJewelStones;
+
+        }
+    }
+}
