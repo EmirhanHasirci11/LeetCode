5f472d879ae81ff48d2bf9c2a8fcf39a15e7d495
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-14 19:17:36 +0300

    1431-KidsWiththeGreatestNumberofCandies

---
+++ 1431-KidsWiththeGreatestNumberofCandies/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace KidsWiththeGreatestNumberofCandies
+{
+    internal class Solution
+    {        
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            List<bool> result = new();
+            int maxCandyCount = candies.Max(); ;
+            foreach(int candy in candies){
+                if (candy + extraCandies >= maxCandyCount)
+                    result.Add(true);
+                else
+                    result.Add(false);
+            }
+            return result;
+        }
+    }
+}
