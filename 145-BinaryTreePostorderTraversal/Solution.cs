02aa7558d4ba005c3c62c11b6ef797a03012f2fb
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-15 15:36:54 +0300

    145-BinaryTreePostorderTraversal

---
+++ 145-BinaryTreePostorderTraversal/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace BinaryTreePostorderTraversal
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        List<int> paths = new List<int>();
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return new List<int>();
+            if (root.left == null && root.right == null)
+                return new List<int> { root.val };
+            postorderTraversal(root);
+            return paths;
+        }
+        public void postorderTraversal(TreeNode node)
+        {
+            if (node == null)
+                return;
+
+            postorderTraversal(node.left);
+            postorderTraversal(node.right);
+            paths.Add(node.val);
+
+        }
+    }
+}
