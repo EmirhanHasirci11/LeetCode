c50a72f613322d4c44c4d3a763faf25ab8de7bb6
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-15 15:17:47 +0300

    94-BinaryTreeInorderTraversal

---
+++ 94-BinaryTreeInorderTraversal/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace BinaryTreePaths
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        List<int> paths = new List<int>();
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return new List<int>();
+            if (root.left == null && root.right == null)
+                return new List<int> { root.val };
+            inorderTraversal(root);
+            return paths;
+        }
+        public void inorderTraversal(TreeNode node)
+        {
+            if (node == null)
+                return;
+
+            inorderTraversal(node.left);
+            paths.Add(node.val);
+            inorderTraversal(node.right);
+
+        }
+    }
+}
