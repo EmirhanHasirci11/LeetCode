77432c3b400bf72db97e3e027753d3761ea5fe76
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-16 11:45:54 +0300

    144-BinaryTreePreorderTraversal

---
+++ 144-BinaryTreePreorderTraversal/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace BinaryTreePreorderTraversal
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        List<int> paths = new List<int>();
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            if (root == null)
+                return new List<int>();
+            if (root.left == null && root.right == null)
+                return new List<int> { root.val };
+            preorderTraversal(root);
+            return paths;
+        }
+        public void preorderTraversal(TreeNode node)
+        {
+            if (node == null)
+                return;
+
+            paths.Add(node.val);
+            preorderTraversal(node.left);
+            preorderTraversal(node.right);
+
+        }
+    }
+}
