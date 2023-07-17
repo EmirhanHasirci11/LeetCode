1cf67b934ab4a4b2ed7d62520472030179fb25d8
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-17 16:51:55 +0300

    1528-ShuffleString

---
+++ 1528-ShuffleString/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace ShuffleString
+{
+    internal class Solution
+    {
+        public string RestoreString(string s, int[] indices)
+        {
+            StringBuilder sb = new StringBuilder(s);
+
+            for (int i = 0; i < s.Length; i++)
+                sb[indices[i]] = s[i];
+
+            return sb.ToString();
+        }
+    }
+}
