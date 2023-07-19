b6fd168a6bd8b32e0cb56064e67f0b03803c4588
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-19 11:50:20 +0300

    242-ValidAnagram

---
+++ 242-ValidAnagram/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace ValidAnagram
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAnagram("anagram","nagaram"));
+        }
+        public static bool IsAnagram(string s, string t)
+        {
+            Dictionary<char,int>map= new ();
+            if (s.Length != t.Length)
+                return false;
+            
+            foreach(char c in s)
+            {
+                if(map.ContainsKey(c))
+                    map[c]++;
+                else
+                    map[c] = 1;
+            }
+            foreach(char c in t)
+            {
+                if (map.ContainsKey(c))
+                {
+                    map[c]--;
+
+                    if (map[c] < 0)
+                        return false;
+                }
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+}
