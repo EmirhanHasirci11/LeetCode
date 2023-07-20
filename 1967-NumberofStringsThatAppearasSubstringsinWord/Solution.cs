9dd85dce20933574858a61f415ffaba3ff2f5c71
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-20 16:09:00 +0300

    1967-NumberofStringsThatAppearasSubstringsinWord

---
+++ 1967-NumberofStringsThatAppearasSubstringsinWord/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace NumberofStringsThatAppearasSubstringsinWord
+{
+    internal class Solution
+    {
+        public int NumOfStrings(string[] patterns, string word)
+        {
+            int count = 0;
+            foreach (string pattern in patterns)
+            {
+                if (word.Contains(pattern))
+                    count++;
+            }
+            return count;
+        }
+    }
+}
