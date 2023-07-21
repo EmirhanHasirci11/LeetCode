590edbed65f732ccf603427bb7799ad7a9efefd7
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-21 17:07:51 +0300

    2154-KeepMultiplyingFoundValuesbyTwo

---
+++ 2154-KeepMultiplyingFoundValuesbyTwo/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace KeepMultiplyingFoundValuesbyTwo
+{
+    internal class Solution
+    {
+        public int FindFinalValue(int[] nums, int original)
+        {
+            HashSet<int> set = nums.ToHashSet();
+
+            while (set.Contains(original))
+                original *= 2;
+
+            return original;
+        }
+    }
+}
