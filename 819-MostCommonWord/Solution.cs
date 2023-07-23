8ce98645c17bef97487c6a23811ed417abc2e307
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-23 15:26:31 +0300

    819-MostCommonWord

---
+++ 819-MostCommonWord/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace MostCommonWord
+{
+    internal class Solution
+    {
+        public string MostCommonWord(string paragraph, string[] banned)
+        {
+            StringBuilder sb = new();
+            string res=string.Empty;
+            int count = 0;
+            foreach (char c in paragraph)
+            {
+                if (char.IsPunctuation(c))
+                    sb.Append(" ");
+                else
+                    sb.Append(char.ToLower(c));
+            }
+            Dictionary<string, int> map = new();
+            string[] words = sb.ToString().Split(" ");
+            foreach (string str in words)
+            {
+                if (str == "") continue;
+                if (banned.Contains(str)) continue;
+                if (map.ContainsKey(str))
+                    map[str]++;
+                else
+                    map[str] = 1;
+                if (map[str] > count)
+                {
+                    count = map[str];
+                    res = str;
+                }
+            }
+            return res;
+
+        }
+    }
+}
