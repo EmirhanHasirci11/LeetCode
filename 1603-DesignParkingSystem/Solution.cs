0aca813d21116d9ee2230b612217d6e334a3396e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-24 18:44:16 +0300

    1603-DesignParkingSystem

---
+++ 1603-DesignParkingSystem/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignParkingSystem
+{
+    public class ParkingSystem
+    {
+        Dictionary<int, int> emptySpace = new();
+        public ParkingSystem(int big, int medium, int small)
+        {
+            emptySpace.Add(1, big);
+            emptySpace.Add(2, medium);
+            emptySpace.Add(3, small);
+        }
+
+        public bool AddCar(int carType)
+        {
+            if (emptySpace[carType] > 0)
+            {
+                emptySpace[carType]--;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
