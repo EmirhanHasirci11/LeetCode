92a0da8e3aac9994cb53e1eb76e3b36b100a292e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-25 11:56:23 +0300

    1287-ElementAppearingMoreThan25PercentInSortedArray

---
+++ 1287-ElementAppearingMoreThan25PercentInSortedArray/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace ElementAppearingMoreThan25PercentInSortedArray
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int FindSpecialInteger(int[] arr)
+        {
+            Dictionary<int, int> map = new();
+            foreach (int val in arr)
+            {
+                if (map.ContainsKey(val))
+                    map[val]++;
+                else 
+                    map[val] = 1;
+            }
+            int percent = arr.Length / 4;
+            foreach(int key in map.Keys)
+            {
+                if (map[key] >= percent)
+                    return key;
+            };
+            return 0;
+        }
+    }
+}
