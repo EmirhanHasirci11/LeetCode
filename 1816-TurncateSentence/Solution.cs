f920042018f99e7130c70ecf165d36397f86f598
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-27 19:53:46 +0300

    1816-TurncateSentence

---
+++ 1816-TurncateSentence/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace TruncateSentence
+{
+    public class Solution
+    {
+        public string TruncateSentence(string s, int k)
+        {
+            StringBuilder sb = new();
+
+            string[] words = s.Split(' ');
+
+            for (int i = 0; i < k - 1; i++)
+                sb.Append(words[i] + " ");
+
+            sb.Append(words[k - 1]);
+
+            return sb.ToString();
+        }
+    }
+}
