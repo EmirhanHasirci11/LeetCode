4df2c877533b91b80de97e68a5a5bfe97e3b9385
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-28 16:52:28 +0300

    441-ArrangingCoins

---
+++ 441-ArrangingCoins/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace ArrangingCoins
+{
+    internal class Solution
+    {
+        public int ArrangeCoins(int n)
+        {
+            int stair = 1;
+            int completedRows = 0;
+            while (n-stair >= 0)
+            {
+                completedRows++;
+                n -= stair;
+                stair++;
+                
+            }
+            return completedRows;
+        }
+    }
+}
