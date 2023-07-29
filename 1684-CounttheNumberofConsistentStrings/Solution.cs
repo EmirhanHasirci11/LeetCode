af14868ef730daefd6daa48f2f6915169060c448
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-07-29 07:47:22 +0300

    1684-CounttheNumberofConsistentStrings

---
+++ 1684-CounttheNumberofConsistentStrings/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace CounttheNumberofConsistentStrings
+{
+    internal class Solution
+    {       
+        public int CountConsistentStrings(string allowed, string[] words)
+        {
+            HashSet<char> set = new(allowed);
+            int result = 0;
+            foreach (string word in words)
+            {
+                bool incremant = true;
+                for (int i = 0; i < word.Length; i++)
+                {
+
+                    if (!set.Contains(word[i]))
+                        incremant = false;
+                }
+                if (incremant == true)
+                    result++;
+
+            }
+            return result;
+
+        }
+    }
+}
