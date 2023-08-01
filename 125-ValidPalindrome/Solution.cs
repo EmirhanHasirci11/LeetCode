05b188fd5ba0eccce501f352da34f65a14b11023
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-01 13:01:57 +0300

    125-ValidPalindrome

---
+++ 125-ValidPalindrome/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace ValidPalindrome
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("A man, a plan, a canal -- Panama"));
+
+        }
+        public static bool IsPalindrome(string s)
+        {
+            var fixedString = s.ToLower().Where(x=>char.IsLetterOrDigit(x));            
+            return fixedString.Reverse().SequenceEqual(fixedString);
+
+        }
+    }
+}
