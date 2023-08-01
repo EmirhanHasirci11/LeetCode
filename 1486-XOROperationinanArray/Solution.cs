c19229f12aa7ee692a122124169ff172ff0fcb72
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-01 12:46:19 +0300

    1486-XOROperationinanArray

---
+++ 1486-XOROperationinanArray/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace XOROperationinanArray
+{
+    internal class Program
+    {
+        public int XorOperation(int n, int start)
+        {
+            int xorSum = start;
+
+            for (int i = 1; i < n; i++)
+                xorSum ^= start + (2 * i);
+
+            return xorSum;
+        }
+    }
+}
