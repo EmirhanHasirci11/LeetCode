b8ad9fb7e58fa041c1829c69a3891b64bbc6e9bd
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-02 09:13:02 +0300

    2283-CheckifNumberHasEqualDigitCountandDigitValue

---
+++ 2283-CheckifNumberHasEqualDigitCountandDigitValue/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace CheckifNumberHasEqualDigitCountandDigitValue
+{
+    internal class Solution
+    {
+        public  bool DigitCount(string num)
+        {
+            Dictionary<double, int> map = new();
+            foreach (char i in num)
+            {
+                var val = Char.GetNumericValue(i);
+                if (map.ContainsKey(val))
+                    map[val]++;
+                else
+                    map[val] = 1;
+            }
+            StringBuilder sb = new ();
+            for(int i=0;i<num.Length;i++)
+            {
+               if(map.ContainsKey(i))
+                    sb.Append(map[i]);
+               else
+                    sb.Append('0');
+            }
+            return sb.ToString()==num;
+        }
+    }
+}
