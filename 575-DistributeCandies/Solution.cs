785071c42e0fe78b994947be89e4930cf1904dff
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-04 16:00:39 +0300

    575-DistributeCandies

---
+++ 575-DistributeCandies/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace DistributeCandies
+{
+    internal class Solution
+    {
+        public int DistributeCandies(int[] candyType)
+        {
+            HashSet<int> set = new(candyType);
+            int candyLimit = candyType.Length/2;
+            int currentCandyCount = 0;
+            foreach(int candy in set)
+            {
+                if(currentCandyCount<candyLimit)
+                    currentCandyCount++;
+            }
+            return currentCandyCount;
+        }
+    }
+}
