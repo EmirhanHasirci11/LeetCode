31e7fd92fec1eea520ddca2f25191ae8376a2c16
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-05 19:19:36 +0300

    2644-FindtheMaximumDivisibilityScore

---
+++ 2644-FindtheMaximumDivisibilityScore/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace FindtheMaximumDivisibilityScore
+{
+    internal class Solution
+    {
+        public int MaxDivScore(int[] nums, int[] divisors)
+        {
+            int maxDivisor = divisors.Min();
+            int resultDivisorCount = 0;
+            foreach(int divisor in divisors)
+            {
+                int totalCount = 0;
+                for(int i=0;i<nums.Length;i++)
+                {
+                    if (nums[i] % divisor == 0)
+                        totalCount++;
+                }
+                if(totalCount>resultDivisorCount || (totalCount==resultDivisorCount && divisor < maxDivisor))
+                {
+                    maxDivisor= divisor;
+                    resultDivisorCount= totalCount;
+                }
+            }
+                return maxDivisor;
+        }
+    }
+}
