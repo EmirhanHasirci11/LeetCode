5ee205f9c5ddccac9f8babc40cbb8af5f7fd3a14
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-06 19:19:22 +0300

    2108-FindFirstPalindromicStringintheArray

---
+++ 2108-FindFirstPalindromicStringintheArray/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace FindFirstPalindromicStringintheArray
+{
+    internal class Solution
+    {
+        public string FirstPalindrome(string[] words)
+        {
+            foreach(string word in words)
+            {
+                if (word.Reverse().SequenceEqual(word))
+                    return word;
+            }
+            return string.Empty;
+        }
+    }
+}
