00b2ae306c0f202dbb069831af66ca93704ef82a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-07 16:56:18 +0300

    2739-TotalDistanceTraveled

---
+++ 2739-TotalDistanceTraveled/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace TotalDistanceTraveled
+{
+    internal class Solution
+    {
+        public int DistanceTraveled(int mainTank, int additionalTank)
+        {
+            int usedFuelCount = 0;
+            int totalDistance = 0;
+            while (mainTank> 0)
+            {
+                totalDistance += 10;
+                usedFuelCount++;
+                mainTank--;
+                if (usedFuelCount==5 && additionalTank>0)
+                {
+                    mainTank++;
+                    additionalTank--;
+                    usedFuelCount = 0;
+                }
+            }
+            return totalDistance;
+        }
+    }
+}
