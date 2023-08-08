2f8c0e7944605cef36302248c03524fd0c7f056e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-08 15:54:54 +0300

    405-ConvertaNumbertoHexadecimal

---
+++ 405-ConvertaNumbertoHexadecimal/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace ConvertaNumbertoHexadecimal
+{
+    internal class Solution
+    {
+
+        private const string hex = "0123456789abcdef";
+
+        public string ToHex(int num)
+        {
+            if (num == 0)
+                return "0";
+
+            var res = new StringBuilder();
+            var n = num > 0 ? num : ((long)2 << 31) + num;
+
+            while (n > 0)
+            {
+                res.Insert(0, hex[(int)(n % 16)]);
+                n /= 16;
+            }
+
+            return res.ToString();
+        }
+    }
+}
