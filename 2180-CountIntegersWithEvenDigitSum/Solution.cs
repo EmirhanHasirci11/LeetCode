3635c7e55bfc983fb0da851c3ef4c9775e3477f8
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-08 14:51:01 +0300

    2180-CountIntegersWithEvenDigitSum

---
+++ 2180-CountIntegersWithEvenDigitSum/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace CountIntegersWithEvenDigitSum
+{
+    internal class Solution
+    {
+        public int CountEven(int num)
+        {
+            int result = 0;
+            while (num > 0)
+            {
+                int digit = num;
+                int sum = 0;
+                while (digit > 0)
+                {
+                    sum += digit % 10;
+                    digit /= 10;
+                }
+                num--;
+                if (sum % 2 == 0)
+                    result++;
+            }
+            return result;
+        }
+    }
+}
