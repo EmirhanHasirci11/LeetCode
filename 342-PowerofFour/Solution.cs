c446813cf0f8712853c894c937d8818a8362ab8f
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-09 15:28:38 +0300

    342-PowerofFour

---
+++ 342-PowerofFour/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace PowerofFour
+{
+    internal class Solution
+    {
+        public bool IsPowerOfFour(int n)
+        {
+            int powerOfFour = 0;
+            while (Math.Pow(4,powerOfFour)!=n)
+            {
+                if (Math.Pow(4,powerOfFour)>n)
+                    return false;
+                powerOfFour++;
+            }
+            return true;
+        }
+    }
+}
