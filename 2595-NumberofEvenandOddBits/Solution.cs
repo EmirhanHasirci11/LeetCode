262a5ae6fcde958d7f05d62feebf41e545205e5d
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-10 16:25:37 +0300

    2595-NumberofEvenandOddBits

---
+++ 2595-NumberofEvenandOddBits/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace NumberofEvenandOddBits
+{
+    internal class Solution
+    {
+        public int[] EvenOddBit(int n)
+        {
+            char[] converted = Convert.ToString(n, 2).ToCharArray();
+            Array.Reverse(converted);
+            int evenIndices = 0;
+            int oddIndices = 0;
+            for (int i = 0; i < converted.Length; i++)
+            {
+                if (converted[i] == '1')
+                {
+                    if (i % 2 == 0)
+                        evenIndices++;
+                    else
+                        oddIndices++;
+                }
+            }
+            return new[] { evenIndices, oddIndices };
+        }
+    }
+}
