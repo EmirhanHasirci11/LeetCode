312e0a54a7ea2f54c92ae1460c485e4ca8ac2862
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-11 15:37:46 +0300

    2309-GreatestEnglishLetterinUpperandLowerCase

---
+++ 2309-GreatestEnglishLetterinUpperandLowerCase/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace GreatestEnglishLetterinUpperandLowerCase
+{
+    internal class Solution
+    {
+        public string GreatestLetter(string s)
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                if (s.Contains((char)('Z' - i)) && s.Contains((char)('z' - i)))
+                    return ((char)('Z' - i)).ToString();
+
+            }
+
+            return string.Empty;
+        }
+    }
+}
