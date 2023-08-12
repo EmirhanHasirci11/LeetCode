7d0f711c4d7c38ad1cd5e539f087296a41c58db4
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-12 16:30:46 +0300

    2614-PrimeInDiagonal

---
+++ 2614-PrimeInDiagonal/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace PrimeInDiagonal
+{
+    internal class Solution
+    {
+        public int DiagonalPrime(int[][] nums)
+        {
+            int max = 0;
+            int lenghtOfNums =nums.Length;
+            for(int i =0;i<lenghtOfNums;i++)
+            {
+                if (IsPrime(nums[i][i]))
+                    max = max > nums[i][i]?max: nums[i][i];
+                if (IsPrime(nums[i][lenghtOfNums - 1 - i]))
+                    max = max > nums[i][lenghtOfNums - 1 - i] ? max : nums[i][lenghtOfNums - 1 - i];
+            }
+            return max;
+        }
+        public bool IsPrime(int x)
+        {
+            if (x == 1)
+                return false;
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
