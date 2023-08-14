9304d05c06c14e11297735156a067d32ea53ae29
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-14 17:18:02 +0300

    506-RelativeRanks

---
+++ 506-RelativeRanks/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace RelativeRanks
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            FindRelativeRanks(new int[] { 5, 4, 3, 2, 1 });
+        }
+        public static string[] FindRelativeRanks(int[] score)
+        {
+            int lengtOfScore = score.Length;
+            string[] result = new string[lengtOfScore];
+            int[] sortedArray = new int[lengtOfScore];
+            Array.Copy(score, sortedArray, lengtOfScore);
+            Array.Sort(sortedArray);
+            Array.Reverse(sortedArray);
+            for (int i = 0; i < lengtOfScore; i++)
+            {
+                string resultText = string.Empty;
+                switch (i)
+                {
+                    case 0:
+                        resultText = "Gold Medal";
+                        break;
+                    case 1:
+                        resultText = "Silver Medal";
+                        break;
+                    case 2:
+                        resultText = "Bronze Medal";
+                        break;
+                    default:
+                        resultText = (i+1).ToString();
+                        break;
+                }
+                result[Array.IndexOf(score,sortedArray[i])] = resultText;
+            }
+            return result;
+        }
+    }
+}
