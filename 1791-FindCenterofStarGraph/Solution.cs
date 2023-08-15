49249291d6f962d6a05113dfd2388a5e0340a43c
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-15 16:34:51 +0300

    1791-FindCenterofStarGraph

---
+++ 1791-FindCenterofStarGraph/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace FindCenterofStarGraph
+{
+    internal class Solution
+    {
+        public int FindCenter(int[][] edges)
+        {
+            int[] nodes = new[] { edges[0][0], edges[0][1], edges[1][0], edges[1][1] };
+
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                for (int j = 0; j < nodes.Length; j++)
+                {
+                    if (i != j && nodes[i] == nodes[j])
+                        return nodes[i];
+
+                }
+            }
+
+            return 0;
+        }
+    }
+}
