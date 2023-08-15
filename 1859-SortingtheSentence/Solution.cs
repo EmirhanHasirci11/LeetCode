431c45d3ceb2787cd8ca1082e08bdb31fbb93853
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-15 16:05:15 +0300

    1859-SortingtheSentence

---
+++ 1859-SortingtheSentence/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace SortingtheSentence
+{
+    internal class Solution
+    {
+
+        public string SortSentence(string s)
+        {
+            string[] words = s.Split(" ");
+            string[] answerArray = new string[words.Length];
+            foreach (string word in words)
+            {
+                answerArray[Convert.ToInt16(word.Substring(word.Length - 1)) - 1] = word.Substring(0, word.Length - 1);
+            }
+            StringBuilder sb = new();
+            foreach (string word in answerArray)
+            {
+                sb.Append(word + " ");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
