3f7b6de3df38bededcc179064a0525c3b449dab0
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-16 22:14:07 +0300

    1331-RankTransformofanArray

---
+++ 1331-RankTransformofanArray/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace RankTransformofanArray
+{
+    internal class Solution
+    {       
+        public int[] ArrayRankTransform(int[] arr)
+        {
+            PriorityQueue<(int index, int value), int> pq = new();
+            int lengthOfArr = arr.Length;
+            for(int i=0;i<lengthOfArr; i++)
+            {
+                pq.Enqueue((i, arr[i]), arr[i]);
+            }
+            int[] result= new int[lengthOfArr];
+            int rank = 1;
+
+            while (pq.Count > 0)
+            {
+                var currentValue = pq.Peek().value;
+
+                while (pq.Count > 0 && pq.Peek().value == currentValue)
+                {
+                    var current = pq.Dequeue();
+                    result[current.index] = rank;
+                }
+
+                rank++;
+            }
+
+            return result;
+        }
+    }
+}
