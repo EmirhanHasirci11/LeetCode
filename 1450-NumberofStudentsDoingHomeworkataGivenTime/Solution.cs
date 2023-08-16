95c57278743bb56bc7f0496154debe197f783044
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-16 16:07:59 +0300

    1450-NumberofStudentsDoingHomeworkataGivenTime

---
+++ 1450-NumberofStudentsDoingHomeworkataGivenTime/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace NumberofStudentsDoingHomeworkataGivenTime
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+        {
+            int countOfValidStudent = 0;
+            for(int i=0;i<startTime.Length;i++)
+            {
+                if (queryTime >= startTime[i] && queryTime <= endTime[i])
+                    countOfValidStudent++;
+            }
+            return countOfValidStudent;
+        }
+    }
+}
