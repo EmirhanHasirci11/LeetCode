1e7a543a19ded02cd38cbc07b7e2364bd832becc
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-16 19:02:34 +0300

    1636-SortArraybyIncreasingFrequency

---
+++ 1636-SortArraybyIncreasingFrequency/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace SortArraybyIncreasingFrequency
+{
+    internal class Solution
+    {       
+        public int[] FrequencySort(int[] nums)
+        {
+            Dictionary<int, int> keyValuePairs = new();
+            foreach (int num in nums)
+            {
+                if (!keyValuePairs.ContainsKey(num))
+                    keyValuePairs[num] = 1;
+                else 
+                    keyValuePairs[num]++;
+            }
+            var i = 0;
+            foreach (var (value, frequency) in keyValuePairs.OrderBy(x => x.Value).ThenByDescending(x => x.Key))
+            {
+                for (var j = 0; j < frequency; j++)
+                {
+                    nums[i++] = value;
+                }
+            }
+            return nums;
+
+
+
+        }
+    }
+}
