d7fb4f3fdf445e1a18e46575b59bd2d85c967e87
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-17 14:14:00 +0300

    2085-CountCommonWordsWithOneOccurrence

---
+++ 2085-CountCommonWordsWithOneOccurrence/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace CountCommonWordsWithOneOccurrence
+{
+    internal class Solution
+    {       
+        public int CountWords(string[] words1, string[] words2)
+        {
+            Dictionary<string, int> map = new();
+            foreach(string word in words1)
+            {
+                if (!map.TryAdd(word, 1))
+                    map[word]++;
+            }
+            foreach(string word in words2)
+            {
+                if (map.ContainsKey(word) && map[word]<=1)
+                    map[word]--;
+            }
+            return map.Count(x => x.Value == 0);
+        }
+    }
+}
