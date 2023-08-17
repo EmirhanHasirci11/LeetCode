8390d7f6a5a4de69d5491f73be44972bea3db0e9
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-17 03:38:40 +0300

    66-PlusOne

---
+++ 66-PlusOne/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace PlusOne
+{
+    internal class Solution
+    {        
+        public int[] PlusOne(int[] digits)
+        {
+            int lengthOfDigits = digits.Length;            
+            for (int i = lengthOfDigits - 1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+            int[] newNumber = new int[lengthOfDigits + 1];
+            newNumber[0] = 1;
+
+            return newNumber;
+        }
+    }
+}
