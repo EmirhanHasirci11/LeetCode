8d0fdd1c95bac718248a280464cc0319a5caac84
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-17 04:11:29 +0300

    226-InvertBinaryTree

---
+++ 226-InvertBinaryTree/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace InvertBinaryTree
+{
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return root;
+            InvertTree(root.left);
+            InvertTree(root.right);
+            (root.left, root.right) = (root.right, root.left);
+            return root;
+        }
+    }
+}
