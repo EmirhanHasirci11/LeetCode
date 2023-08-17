75e858b25d61d60e38683e4c98834d960bbf514e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-17 03:47:36 +0300

    674-LongestContinuousIncreasingSubsequence

---
+++ 674-LongestContinuousIncreasingSubsequence/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LongestContinuousIncreasingSubsequence
+{
+    internal class Solution
+    {               
+        public int FindLengthOfLCIS(int[] nums)
+        {
+            int maxSubsequence = 1;
+            int iterator = 1;
+            int prevNum = nums[0];
+            foreach (int num in nums)
+            {
+                if (prevNum < num)
+                {
+                    iterator++;
+
+                    if (maxSubsequence < iterator)
+                        maxSubsequence = iterator;
+                }
+                else
+                    iterator = 1;
+                prevNum= num;
+            }
+            return maxSubsequence;
+        }
+    }
+}
