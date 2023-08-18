ea4c92b6df74e239745a8fda29c94c2170acd8da
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-18 18:11:17 +0300

    1979-FindGreatestCommonDivisorofArray

---
+++ 1979-FindGreatestCommonDivisorofArray/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace FindGreatestCommonDivisorofArray
+{
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            FindGCD(new int[] { 252,105 });
+        }
+        public static int FindGCD(int[] nums)
+        {            
+            return gcd(nums.Min(), nums.Max()); 
+        }
+        private static int gcd(int a,int b)
+        {
+            if (b == 0) return a;
+            return gcd(b, a % b);
+        }
+    }
+}
