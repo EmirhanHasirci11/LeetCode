65c3a0d6cacf2c6cd9c1c830560175630902ad86
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-19 14:22:42 +0300

    744-FindSmallestLetterGreaterThanTarget

---
+++ 744-FindSmallestLetterGreaterThanTarget/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace FindSmallestLetterGreaterThanTarget
+{
+    internal class Solution
+    {       
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            char greaterThanTarget = letters[0];
+            bool firstGreaterSelected = false;
+            foreach (char c in letters)
+            {
+                if (c > target)
+                {
+                    if (firstGreaterSelected == false)
+                    {
+                        greaterThanTarget = c;
+                        firstGreaterSelected= true;
+                    }
+                    if (c < greaterThanTarget)
+                        greaterThanTarget = c;
+                }
+
+            }
+            return greaterThanTarget;
+        }
+    }
+}
