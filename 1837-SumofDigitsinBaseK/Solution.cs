c19e7a565c00622cce48167a32b722df77690b56
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-20 19:23:22 +0300

    1837-SumofDigitsinBaseK

---
+++ 1837-SumofDigitsinBaseK/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace SumofDigitsinBaseK
+{
+    internal class Solution
+    {        
+        public int SumBase(int n, int k)
+        {
+            int sumOfConvertedDigits = 0;
+            while (n > 0)
+            {
+                sumOfConvertedDigits+= n%k;
+                n/= k;
+            }                  
+            return sumOfConvertedDigits;
+        }
+    }
+}
