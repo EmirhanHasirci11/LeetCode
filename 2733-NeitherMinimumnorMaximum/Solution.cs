8fcf32dc73ef9d6120e708b21647779844493cff
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-20 18:55:47 +0300

    2733-NeitherMinimumnorMaximum

---
+++ 2733-NeitherMinimumnorMaximum/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace NeitherMinimumnorMaximum
+{
+    internal class Solution
+    {        
+        public int FindNonMinOrMax(int[] nums)
+        {
+            if (nums.Length <= 2)
+                return -1;
+            
+            Array.Sort<int>(nums);
+            return nums[1];
+        }
+    }
+}
