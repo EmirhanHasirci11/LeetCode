f6734c90d00f6e02e0aed4ea0f051274b189e111
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-21 15:33:09 +0300

    804-UniqueMorseCodeWords

---
+++ 804-UniqueMorseCodeWords/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace UniqueMorseCodeWords
+{
+    internal class Solution
+    {        
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            string[] mors = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            Dictionary<string,int>map= new ();
+            foreach(string word in words)
+            {
+                StringBuilder sb= new StringBuilder();
+                foreach(char c in word)
+                {
+                    sb.Append(mors[c-'a']);
+                }
+                if(!map.TryAdd(sb.ToString(),1))
+                    map[sb.ToString()]++;
+            }
+            return map.Count();
+
+        }
+    }
+}
