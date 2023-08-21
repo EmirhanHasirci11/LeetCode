ed6027fe5e8f188631be8abf3f8235caaf900a54
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-21 20:13:22 +0300

    459-RepeatedSubstringPattern

---
+++ 459-RepeatedSubstringPattern/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace RepeatedSubstringPattern
+{
+    internal class Solution
+    {       
+        public bool RepeatedSubstringPattern(string s)
+        {
+            int lengthOfS = s.Length;
+            for (int i = 1; i <= lengthOfS / 2; i++)
+            {
+                if (lengthOfS % i == 0)
+                {
+                    string substr = s.Substring(0, i);
+                    StringBuilder sb = new();
+                    for (int j = 0; j < lengthOfS / i; j++)
+                        sb.Append(substr);
+                    if (sb.ToString() == s)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
