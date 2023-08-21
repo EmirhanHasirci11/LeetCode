c90fa612cf58cef4a47e9fc4ff6244e2c2a43c68
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-21 15:43:50 +0300

    2656-MaximumSumWithExactlyKElements

---
+++ 2656-MaximumSumWithExactlyKElements/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace MaximumSumWithExactlyKElements
+{
+    internal class Solution
+    {
+        public int MaximizeSum(int[] nums, int k)
+        {
+            int max = nums.Max();
+            int count = max;
+            for(int i=1;i<k;i++)
+            {
+                max += 1;
+                count += max;
+
+            }
+            return count;
+        }
+    }
+}
