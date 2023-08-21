234df888eb1f2e54a2dd8b4c688636c98cd2eee7
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-21 16:10:25 +0300

    2490-CircularSentence

---
+++ 2490-CircularSentence/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace CircularSentence
+{
+    internal class Solution
+    {        
+        public bool IsCircularSentence(string sentence)
+        {
+            string[] words =sentence.Split(' ');
+            if (words.Length == 1 && words[0][0] == words[words.Length-1][words[words.Length - 1].Length - 1])
+                return true;
+            
+            if (words[0][0] != words[words.Length - 1][words[words.Length-1].Length - 1])
+                return false;
+
+            bool control=true;
+
+            for(int i=0;i<words.Length-1;i++) 
+            {
+                char lastDigit = words[i][words[i].Length-1];
+                if (lastDigit != words[i + 1][0])
+                    return false;
+            }
+
+            return control;
+        }
+    }
+}
