9c6d01b868c00ac5626de7bd5e35a2d3d3e95203
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-21 19:17:56 +0300

    1572-MatrixDiagonalSum

---
+++ 1572-MatrixDiagonalSum/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace MatrixDiagonalSum
+{
+    internal class Solution
+    {        
+        public int DiagonalSum(int[][] mat)
+        {            
+            int sumOfDiagonals = 0;
+            for(int i=0;i<mat.Length;i++)
+            {
+                sumOfDiagonals+= mat[i][i] + mat[i][mat.Length-1-i];
+            }
+            if(mat.Length % 2!=0)
+                sumOfDiagonals -= mat[(mat.Length - 1)/2][(mat.Length - 1) / 2];
+            return sumOfDiagonals;
+        }
+    }
+}
