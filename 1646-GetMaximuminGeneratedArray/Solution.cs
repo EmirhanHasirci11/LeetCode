9d32678afc0d5360229af3fc553dab0ed859e716
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-22 15:49:01 +0300

    1646-GetMaximuminGeneratedArray

---
+++ 1646-GetMaximuminGeneratedArray/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace GetMaximuminGeneratedArray
+{
+    internal class Solution
+    {       
+        public int GetMaximumGenerated(int n)
+        {
+            if (n == 0)
+                return 0;
+            int[] arr = new int[n + 1];
+            arr[0] = 0;
+            arr[1] = 1;
+            int i = 1;
+            while (i < arr.Length / 2)
+            {
+                arr[i * 2] = arr[i];
+                arr[(i * 2) + 1] = arr[i] + arr[i + 1];
+                i++;
+            }
+            return arr.Max();
+        }
+    }
+}
