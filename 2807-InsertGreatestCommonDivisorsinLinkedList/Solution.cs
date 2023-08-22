1f3a017114ce6fccc9e7a8378e75e89a096e2947
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-22 17:41:18 +0300

    2807-InsertGreatestCommonDivisorsinLinkedList

---
+++ 2807-InsertGreatestCommonDivisorsinLinkedList/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace InsertGreatestCommonDivisorsinLinkedList
+{
+    internal class Solution
+    {        
+        public ListNode InsertGreatestCommonDivisors(ListNode head)
+        {
+            ListNode headDummy= head;
+            ListNode current = headDummy.next;
+            while(current != null) 
+            {
+                int gcdValue = GCD(headDummy.val, current.val);
+                ListNode node = new(gcdValue,current);
+                headDummy.next = node;
+                headDummy= current;
+                current = current.next;
+            }
+            return head;
+        }
+        public int GCD(int a, int b)
+        {
+            if (b > a)
+                return GCD(b, a);
+            if (a % b == 0)
+                return b;
+            return GCD(b, a % b);
+        }
+    }
+}
