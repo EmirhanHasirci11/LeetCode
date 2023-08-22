08003ed515186aa5588302a2ff5803b4010f4341
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-22 20:55:07 +0300

    2828-CheckifaStringIsanAcronymofWords

---
+++ 2828-CheckifaStringIsanAcronymofWords/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace CheckifaStringIsanAcronymofWords
+{
+    internal class Solution
+    {        
+        public bool IsAcronym (IList<string> words, string s)
+        {
+            if (words.Count != s.Length)
+                return false;
+            for(int i=0;i<words.Count;i++)
+            {
+                if (!words[i].StartsWith(s[i]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
