a88647a552ca417f17dc977b08c5db881e9ef84e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-22 15:12:55 +0300

    168-ExcelSheetColumnTitle

---
+++ 168-ExcelSheetColumnTitle/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace ExcelSheetColumnTitle
+{
+    internal class Solution
+    {        
+        public string ConvertToTitle(int columnNumber)
+        {
+            StringBuilder sb = new();
+            while (columnNumber > 0)
+            {
+                int remainder = (columnNumber - 1)%26;
+                char c = (char)('A' + remainder);
+                sb.Insert(0, c);
+                columnNumber=(columnNumber-1)/26;
+            }
+            return sb.ToString();
+        }
+    }
+}
