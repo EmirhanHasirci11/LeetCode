a83651d20d3846894035fe693ae33a93b7e94319
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-22 17:29:44 +0300

    2710-RemoveTrailingZerosFromaString

---
+++ 2710-RemoveTrailingZerosFromaString/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace RemoveTrailingZerosFromaString
+{
+    internal class Solution
+    {        
+        public string RemoveTrailingZeros(string num)
+        {
+            int countOfZeros = 0;
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                if (num[i] == '0')
+                    countOfZeros++;
+                else break;                
+            }
+                
+            return num.Substring(0, num.Length-1-countOfZeros);
+        }
+    }
+}
