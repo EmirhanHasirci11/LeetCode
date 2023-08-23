58d199397b16e9805a1e394233a6ed3a241582f4
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-23 22:14:49 +0300

    414-ThirdMaximumNumber

---
+++ 414-ThirdMaximumNumber/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace ThirdMaximumNumber
+{
+    internal class Solution
+    {
+        public int ThirdMax(int[] nums)
+        {
+            HashSet<int> set = new();
+            foreach (int num in nums)
+            {
+                set.Add(num);
+                if (set.Count > 3) set.Remove(set.Min());
+            }
+            if (set.Count < 3) return set.Max();
+            return set.Min();
+        }
+    }
+}
