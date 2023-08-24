1e3769a9c67f92e24ffc47e5bb295883df771243
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-24 14:46:49 +0300

    2545-SorttheStudentsbyTheirKthScore

---
+++ 2545-SorttheStudentsbyTheirKthScore/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace SorttheStudentsbyTheirKthScore
+{
+    internal class Solution
+    {        
+        public int[][] SortTheStudents(int[][] score, int k)
+        {
+            int studentCount=score.Length;
+            int examCount = score[0].Length;
+            List<(int score,int index)> kCol=new();
+            for(int i=0;i<studentCount;i++)
+            {
+                kCol.Add(new(score[i][k], i));
+            }
+            kCol.Sort((x, y) => y.score.CompareTo(x.score));
+            int[][] result = new int[studentCount][];
+            for(int i=0;i<studentCount ;i++) 
+            {
+                result[i]=new int[examCount];
+                for (int j = 0; j < examCount; j++)
+                    result[i][j] = score[kCol[i].index][j];
+            }
+            return result;
+        }
+    }
+}
