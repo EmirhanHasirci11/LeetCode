9989af4222dbc489f30744a9b349fb71cadb68b6
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-25 17:51:47 +0300

    1018-BinaryPrefixDivisibleBy5

---
+++ 1018-BinaryPrefixDivisibleBy5/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace BinaryPrefixDivisibleBy5
+{
+    internal class Solution
+    {        
+        public IList<bool> PrefixesDivBy5(int[] nums)
+        {
+            int x = 0;
+            bool[] res= new bool[nums.Length];
+            int index = 0;
+            while (index < nums.Length)
+            {
+                x = (x * 2 + nums[index]) % 10;
+                res[index] = (x%5==0);
+                index++;
+            }
+            return res;
+        }
+    }
+}
