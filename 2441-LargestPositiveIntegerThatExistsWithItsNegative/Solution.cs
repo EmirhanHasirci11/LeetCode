2c5cf3025fac989d09b1681598c7b52eef6650d2
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-26 16:10:00 +0300

    2441-LargestPositiveIntegerThatExistsWithItsNegative

---
+++ 2441-LargestPositiveIntegerThatExistsWithItsNegative/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LargestPositiveIntegerThatExistsWithItsNegative
+{
+    internal class Solution
+    {
+        public int FindMaxK(int[] nums)
+        {
+            nums = nums.OrderByDescending(x => x).ToArray();
+            int i = 0, j = nums.Length - 1;
+            while (i < j)
+            {
+                if (nums[i] == -1 * nums[j])
+                    return nums[i];
+                else if (nums[i] > -1 * nums[j])
+                    i++;
+                else
+                    j--;
+            }
+            return -1;
+        }
+    }
+}
