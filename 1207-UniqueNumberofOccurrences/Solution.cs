25c364f7156101eb6fa4ae2b2a14bac10975241f
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-27 15:27:40 +0300

    1207-UniqueNumberofOccurrences

---
+++ 1207-UniqueNumberofOccurrences/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace UniqueNumberofOccurrences
+{
+    internal class Solution
+    {        
+        public bool UniqueOccurrences(int[] arr)
+        {
+            Dictionary<int, int> map = new();
+            foreach(int num in arr)
+            {
+                if (!map.TryAdd(num, 1))
+                    map[num]++;
+            }
+            HashSet<int> set = new(map.Values);
+            return map.Count() == set.Count();
+        }
+    }
+}
