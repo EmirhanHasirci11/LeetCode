4603856cfc6dda7a2600e891f1971b9722123aed
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-27 04:21:17 +0300

    2053-KthDistinctStringinanArray

---
+++ 2053-KthDistinctStringinanArray/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace KthDistinctStringinanArray
+{
+    internal class Solution
+    {        
+        public  string KthDistinct(string[] arr, int k)
+        {
+            Dictionary<string, int> map = new();
+            foreach (string c in arr)
+            {
+                if(!map.TryAdd(c,1))
+                    map[c]++;
+            }
+
+            int count = 0;
+
+            foreach(string c in map.Keys)
+            {
+                if (map[c] == 1)
+                    count++;
+                if(count==k)
+                    return c;
+            }
+            return string.Empty;
+            
+        }
+    }
+}
