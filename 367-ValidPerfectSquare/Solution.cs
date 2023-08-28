c8600e01ed2b8755cf80e79486443335aab835f9
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-28 03:15:38 +0300

    367-ValidPerfectSquare

---
+++ 367-ValidPerfectSquare/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace ValidPerfectSquare
+{
+    internal class Solution
+    {        
+        public bool IsPerfectSquare(int num)
+        {
+            long a = 1;
+            long b = num;
+            while (a <= b)
+            {
+                long mid = a + (b - a) / 2;
+                long mult = mid * mid;
+                if (mult > num)
+                    b = mid - 1;
+                else if (mult < num)
+                    a = mid + 1;
+                else return true;
+            }
+            return false;
+        }
+}
