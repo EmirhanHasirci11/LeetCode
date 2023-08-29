38007548cdc3d4f07a9f4f7e5cfb2ab8791dc906
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-29 03:55:10 +0300

    1470-ShuffletheArray

---
+++ 1470-ShuffletheArray/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace ShuffletheArray
+{
+    internal class Solution
+    {        
+        public int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[2*n];            
+            int leftIndexer = 0;
+            for (int i = 0; i<result.Length;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result[i] = nums[leftIndexer];
+                    leftIndexer++;
+                }
+                else
+                {
+                    result[i] = nums[n];
+                    n++;
+                }
+            }
+            return result;
+        }
+    }
+}
