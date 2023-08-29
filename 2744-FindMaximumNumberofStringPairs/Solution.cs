458f79def858d091f140204add885f751ca2811e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-29 10:36:36 +0300

    2744-FindMaximumNumberofStringPairs

---
+++ 2744-FindMaximumNumberofStringPairs/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace FindMaximumNumberofStringPairs
+{
+    internal class Solution
+    {        
+        public int MaximumNumberOfStringPairs(string[] words)
+        {
+            Dictionary<string, bool> map = new();
+            foreach (string word in words)
+            {
+                if (map.ContainsKey("" + word[1] + word[0]) || map.ContainsKey(word))
+                {
+                    if (!map.TryGetValue(word, out bool val))
+                        map["" + word[1] + word[0]] = true;
+
+                    else
+                        map[word] = true;
+                }
+                else map[word] = false;
+            }
+            return map.Where(x => x.Value == true).Count();
+        }
+    }
+}
