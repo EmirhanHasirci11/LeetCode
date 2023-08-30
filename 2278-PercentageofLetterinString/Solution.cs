395425e3066060bda5bd2596243e0a32f48b3694
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-30 16:33:55 +0300

    2278-PercentageofLetterinString

---
+++ 2278-PercentageofLetterinString/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace PercentageofLetterinString
+{
+    internal class Solution
+    {        
+        public int PercentageLetter(string s, char letter)
+        {
+            int count = 0;
+            int lengthOfs = s.Length;
+            for(int i = 0; i < lengthOfs; i++)
+            {
+                if (s[i] == letter)
+                    count++;
+            }
+            return count == 0 ? 0 :(count*100)/lengthOfs;
+        }
+    }
+}
