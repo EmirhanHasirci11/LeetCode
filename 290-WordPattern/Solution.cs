a7f2aac4e9a982dd66b948d1e1550b1059dd1d3b
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-30 03:14:58 +0300

    290-WordPattern

---
+++ 290-WordPattern/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace WordPattern
+{
+    internal class Solution
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            string[] sArray = s.Split();
+
+            if (pattern.Length != sArray.Length)
+                return false;
+
+            Dictionary<char, string> map = new();
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (map.ContainsKey(pattern[i]))
+                {
+                    if (map[pattern[i]] != sArray[i]) return false;
+                }
+                else
+                {
+                    if (map.ContainsValue(sArray[i])) return false;
+                    map.Add(pattern[i], sArray[i]);
+                }
+            }
+            return true;
+        }
+    }
+}
