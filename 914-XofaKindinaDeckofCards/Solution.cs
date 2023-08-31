a2eb9a6f4e12b49e27c4f74708f7cae7158bebde
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-08-31 14:15:04 +0300

    914-XofaKindinaDeckofCards

---
+++ 914-XofaKindinaDeckofCards/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace XofaKindinaDeckofCards
+{
+    internal class Solution
+    {        
+        public bool HasGroupsSizeX(int[] deck)
+        {            
+            Dictionary<int, int> map = new();
+            foreach (int i in deck)
+            {
+                if (!map.TryAdd(i, 1))
+                    map[i]++;
+            }
+            int minimumPairCount = 0;
+            foreach (int mapValues in map.Values)
+            {
+                minimumPairCount = GCD(minimumPairCount, mapValues);
+                if (minimumPairCount < 2) return false;
+            }
+            return true;
+        }
+        private int GCD(int a, int b)
+        {
+            if (b > a) return GCD(b, a);
+            if (a % b == 0) return b;
+            return GCD(b, a % b);
+        }
+    }
+}
