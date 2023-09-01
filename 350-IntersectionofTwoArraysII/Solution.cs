86b145c2778c687e93af370a4341e830d3e3d730
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-01 10:30:21 +0300

    350-IntersectionofTwoArraysII

---
+++ 350-IntersectionofTwoArraysII/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace IntersectionofTwoArraysII
+{
+    internal class Solution
+    {        
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            Dictionary<int,int> map = new();
+            foreach(int i in nums1)
+            {
+                if (!map.TryAdd(i, 1))
+                    map[i]++;
+            }
+            List<int> list = new();
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                if (map.ContainsKey(nums2[i]) && map[nums2[i]] > 0)
+                {
+                    map[nums2[i]]--;
+                    list.Add(nums2[i]);
+                }
+
+            }
+
+            return list.ToArray();
+
+
+        }
+    }
+}
