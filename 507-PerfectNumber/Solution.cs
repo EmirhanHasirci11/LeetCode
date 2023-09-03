3a39cab1a2844d9664954f7fc7ce8fb96f93c7bf
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-03 08:41:13 +0300

    507-PerfectNumber

---
+++ 507-PerfectNumber/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace PerfectNumber
+{
+    internal class Solution
+    {        
+        public bool CheckPerfectNumber(int num)
+        {
+            int sumOfDivisors = 0;
+            for(int i=1;i<num/2;i++)
+            {
+                sumOfDivisors += num % i == 0 ? i:0;
+            }
+            return num == sumOfDivisors;
+        }
+    }
+}
