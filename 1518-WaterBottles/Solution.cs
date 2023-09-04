13c803821582d5ad90fab786de862b2ac91c409d
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-04 21:44:27 +0300

    1518-WaterBottles

---
+++ 1518-WaterBottles/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace WaterBottles
+{
+    internal class Solution
+    {
+        public int NumWaterBottles(int numBottles, int numExchange)
+        {
+            int amountOfDrinkedBottle = numBottles;
+            int emptyBottles = 0;
+
+            while (numBottles >= numExchange)
+            {
+                emptyBottles = numBottles % numExchange;
+                numBottles -= emptyBottles;
+                amountOfDrinkedBottle += numBottles / numExchange;
+                numBottles = (numBottles / numExchange) + emptyBottles;
+
+            }
+            return amountOfDrinkedBottle;
+        }
+    }
+}
