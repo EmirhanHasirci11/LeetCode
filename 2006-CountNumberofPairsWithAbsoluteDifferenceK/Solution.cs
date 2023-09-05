83388851bd0b5bfb35edd8017556e0fd4dd98772
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-05 20:26:30 +0300

    2006-CountNumberofPairsWithAbsoluteDifferenceK

---
+++ 2006-CountNumberofPairsWithAbsoluteDifferenceK/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace CountNumberofPairsWithAbsoluteDifferenceK
+{
+    internal class Solution
+    {        
+        public int CountKDifference(int[] nums, int k)
+        {
+            int countOfKDifferencePairs = 0;
+
+            for(int i=0;i<nums.Length;i++)            
+                for(int j=i+1;j<nums.Length;j++)                
+                    if (Math.Abs(nums[i] - nums[j])==k)
+                        countOfKDifferencePairs++;
+                            
+            return countOfKDifferencePairs;
+        }
+    }
+}
