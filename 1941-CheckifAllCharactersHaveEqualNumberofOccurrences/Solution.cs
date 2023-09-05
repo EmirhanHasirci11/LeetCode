bb3fbbe1440b1eff049de4a38012fe17c91eb3f6
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-05 20:19:55 +0300

    1941-CheckifAllCharactersHaveEqualNumberofOccurrences

---
+++ 1941-CheckifAllCharactersHaveEqualNumberofOccurrences/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace CheckifAllCharactersHaveEqualNumberofOccurrences
+{
+    internal class Solution
+    {
+        public bool AreOccurrencesEqual(string s)
+        {
+            Dictionary<char, int> map = new();
+
+            foreach (char c in s)
+                if (!map.TryAdd(c, 1))
+                    map[c]++;
+
+            int countOfFirstC = map[s[0]];
+
+            foreach (int val in map.Values)
+                if (val != countOfFirstC)
+                    return false;
+
+            return true;
+        }
+    }
+}
