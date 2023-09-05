aad89ecedaa4ff397657bb30cba48f54940f969a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-05 16:24:44 +0300

    268-MissingNumber

---
+++ 268-MissingNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MissingNumber
+{
+    internal class Program
+    {        
+        public int MissingNumber(int[] nums)
+        {
+            int xor = 0;
+            foreach(int i in nums)
+                xor ^= i;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                xor ^= nums[i];
+            }
+            return xor;
+        }
+    }
+}
