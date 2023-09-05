772b18f394f83935e8726c30c27662eceb90925d
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-05 16:12:39 +0300

    2000-ReversePrefixofWord

---
+++ 2000-ReversePrefixofWord/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace ReversePrefixofWord
+{
+    internal class Solution
+    {
+        public string ReversePrefix(string word, char ch)
+        {
+            if (!word.Contains(ch))
+                return word;
+            string result = "";
+            foreach (char c in word)
+            {
+                result = c + result;
+                if (c == ch)
+                    break;
+            }
+            
+            return result + word.Substring(result.Length);
+        }
+    }
+}
