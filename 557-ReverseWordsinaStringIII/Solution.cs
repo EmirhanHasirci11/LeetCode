fa8e6e41702f6072cdf6790555c2e74627536c0e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-06 13:35:33 +0300

    557-ReverseWordsinaStringIII

---
+++ 557-ReverseWordsinaStringIII/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace ReverseWordsinaStringIII
+{
+    internal class Solution
+    {        
+        public string ReverseWords(string s)
+        {
+            string[] arrayOfS = s.Split();
+            StringBuilder sb = new();
+            foreach(string word in arrayOfS)
+            {
+                for(int i = word.Length - 1; i >= 0; i--)                
+                    sb.Append(word[i]);
+                
+                sb.Append(" ");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
