ba96b6c4c47ca4d3fd511d87be38f2c39364a046
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-06 13:50:11 +0300

    344-ReverseString

---
+++ 344-ReverseString/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace ReverseString
+{
+    internal class Solution
+    {       
+        public void ReverseString(char[] s)
+        {
+            int halfLengthOfS = s.Length % 2 == 0 ? s.Length / 2 : (s.Length - 1) / 2;
+            for(int i=0;i<halfLengthOfS;i++)
+            {
+                char temp = s[i];
+                s[i] = s[s.Length - 1 - i];
+                s[s.Length - 1 - i]= temp;
+            }           
+        }
+    }
+}
