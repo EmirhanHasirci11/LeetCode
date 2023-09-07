13d42a11eb4cba9a0e12a88ba11e22ec3d2cf0ec
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-07 08:10:32 +0300

    1894-FindtheStudentthatWillReplacetheChalk

---
+++ 1894-FindtheStudentthatWillReplacetheChalk/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace FindtheStudentthatWillReplacetheChalk
+{
+    internal class Solution
+    {        
+        public int ChalkReplacer(int[] chalk, int k)
+        {
+            long sumOfChalk = 0;
+            for(int i = 0; i < chalk.Length; i++)
+            {
+                sumOfChalk += chalk[i];
+            }
+            long modularOfChalk = k%sumOfChalk;
+
+            for(int i=0;i<chalk.Length; i++)
+            {
+                if (chalk[i]>modularOfChalk)
+                    return i;
+                modularOfChalk-= chalk[i];
+            }
+            return 0;
+        }
+    }
+}
