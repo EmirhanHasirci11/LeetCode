f61b830febc7002b65913f1e3b9b96f5541fcde6
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-08 11:08:31 +0300

    693-BinaryNumberwithAlternatingBits

---
+++ 693-BinaryNumberwithAlternatingBits/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace BinaryNumberwithAlternatingBits
+{
+    internal class Solution
+    {        
+        public bool HasAlternatingBits(int n)
+        {
+            string converted =Convert.ToString(n,2);
+
+            for(int i = 1; i < converted.Length; i++)            
+                if (converted[i - 1] == converted[i])
+                    return false;
+            
+            return true;
+
+        }
+    }
