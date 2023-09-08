d6bf7e9a6b82fba3bf77f48a8d716d37823c76bf
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-08 11:38:51 +0300

    1805-NumberofDifferentIntegersinaString

---
+++ 1805-NumberofDifferentIntegersinaString/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace NumberofDifferentIntegersinaString
+{
+    internal class Solution
+    {        
+        public int NumDifferentIntegers(string word)
+        {
+            HashSet<string> h = new();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (Char.IsDigit(word[i]))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    while (i < word.Length && Char.IsDigit(word[i]))
+                    {
+                        sb.Append(word[i]);
+                        i++;
+                    }
+
+                    h.Add(sb.ToString().TrimStart('0'));
+                }
+            }
+
+            return h.Count;
+        }
+    }
+}
