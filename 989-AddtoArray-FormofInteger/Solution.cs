40a8a5a4b437592b8aaf86768a4fe2f7bf571870
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-08 11:04:18 +0300

    989-AddtoArray-FormofInteger

---
+++ 989-AddtoArray-FormofInteger/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace AddtoArray_FormofInteger
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            AddToArrayForm(new int[] { 2, 7, 4 }, 181);
+        }
+        public static IList<int> AddToArrayForm(int[] num, int k)
+        {
+            List<int> res = new();
+            for (int i = num.Length - 1; i >= 0; --i)
+            {
+                res.Insert(0, (num[i] + k) % 10);
+                k = (num[i] + k) / 10;
+            }
+            while (k > 0)
+            {
+                res.Insert(0, k % 10);
+                k /= 10;
+            }
+            return res;
+        }
+    }
+}
