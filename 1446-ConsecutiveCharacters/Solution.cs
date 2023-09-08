19d063ab8a15a8e8e6d2ca5786b2ec5876cb823c
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-08 10:44:21 +0300

    ConsecutiveCharacters

---
+++ 1446-ConsecutiveCharacters/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsecutiveCharacters
+{
+    internal class Solution
+    {        
+        public int MaxPower(string s)
+        {
+           
+            int maxUniqueSubstringCount =1;
+            int currentUniqueSub = 1;
+            char currentChar = s[0];
+            for(int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == currentChar)
+                    currentUniqueSub++;
+                else
+                {
+                    maxUniqueSubstringCount = Math.Max(currentUniqueSub, maxUniqueSubstringCount);
+                    currentUniqueSub = 1;
+                }
+                currentChar = s[i];
+            }
+            return maxUniqueSubstringCount;
+        }
+    }
+}
