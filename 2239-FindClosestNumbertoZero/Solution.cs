38bc3605139dad7d19c735a64c1e818abe170f87
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-09 13:12:57 +0300

    2239-FindClosestNumbertoZero

---
+++ 2239-FindClosestNumbertoZero/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace FindClosestNumbertoZero
+{
+    internal class Solution
+    {        
+        public int FindClosestNumber(int[] nums)
+        {
+            int closestValue = int.MaxValue;
+            int index = 0;
+            foreach(int i in nums)
+            {
+                if (Math.Abs(i) < closestValue || i==Math.Abs(closestValue))
+                {
+                    closestValue = Math.Abs(i);
+                    index = i;
+                }
+            }
+            return index;
+        }
+    }
+}
