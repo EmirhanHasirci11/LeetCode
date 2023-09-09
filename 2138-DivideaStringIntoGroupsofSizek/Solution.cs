91ed6b17298edf945dd5805978285dda6938038a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-09 13:35:03 +0300

    2138-DivideaStringIntoGroupsofSizek

---
+++ 2138-DivideaStringIntoGroupsofSizek/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace DivideaStringIntoGroupsofSizek
+{
+    internal class Solution
+    {        
+        public string[] DivideString(string s, int k, char fill)
+        {
+            StringBuilder sb = new();
+            List<string> list = new();
+            for (int i = 1; i <= s.Length; i++)
+            {
+                sb.Append(s[i - 1]);
+                if (i % k == 0)
+                {
+                    list.Add(sb.ToString());
+                    sb = new StringBuilder();
+                }
+            }
+            if (sb.Length != k && sb.Length != 0)
+            {
+                while (sb.Length < k)
+                    sb.Append(fill);
+
+                list.Add(sb.ToString());
+            }
+            return list.ToArray();
+        }
+    }
+}
