70629cfde3ebde503534b968a574aa0895b66c7a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-10 16:30:39 +0300

    520-DetectCapital

---
+++ 520-DetectCapital/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace DetectCapital
+{
+    internal class Solution
+    {        
+        public bool DetectCapitalUse(string word)
+        {
+            int countOfCapital = 0;
+            for(int i=0;i<word.Length;i++)            
+                if (char.IsUpper(word[i]))
+                    countOfCapital++;
+            
+            return countOfCapital == word.Length ? true : countOfCapital == 1 && char.IsUpper(word[0])?true:countOfCapital==0?true:false;
+        }
+    }
+}
