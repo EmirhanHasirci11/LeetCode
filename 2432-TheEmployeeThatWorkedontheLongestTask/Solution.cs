b08880b705a3ecf5180ee08d38d8b233786834d7
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-10 16:57:29 +0300

    2432-TheEmployeeThatWorkedontheLongestTask

---
+++ 2432-TheEmployeeThatWorkedontheLongestTask/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace TheEmployeeThatWorkedontheLongestTask
+{
+    internal class Solution
+    {
+        public int HardestWorker(int n, int[][] logs)
+        {
+            int maxUnitTime = 0,workerId = 0,latestWorkTime = 0;
+            for (int i = 0; i < logs.Length; i++)
+            {
+                if ((logs[i][1]-latestWorkTime)>maxUnitTime|| ((logs[i][1] - latestWorkTime) == maxUnitTime && workerId > logs[i][0]))
+                {
+                    maxUnitTime = logs[i][1]-latestWorkTime;
+                    workerId = logs[i][0];
+                }
+                latestWorkTime = logs[i][1];
+            }
+            return workerId;
+        }
+    }
+}
