98bc0808f861a1bda9133bd47921645bcb3558fb
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-11 20:09:45 +0300

    2520-CounttheDigitsThatDivideaNumber

---
+++ 2520-CounttheDigitsThatDivideaNumber/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace CounttheDigitsThatDivideaNumber
+{
+    internal class Solution
+    {        
+        public int CountDigits(int num)
+        {
+            int tempOfNum = num;
+            int countOfDivisibleDigit = 0;
+            while (tempOfNum > 0)
+            {
+                if(num%(tempOfNum%10)==0)
+                    countOfDivisibleDigit++;
+                tempOfNum /= 10;
+            }
+            return countOfDivisibleDigit;
+        }
+    }
+}
