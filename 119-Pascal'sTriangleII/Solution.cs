9fe32b4351415162dcf81b3ac326e66122abdd60
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-11 15:12:06 +0300

    119-Pascal'sTriangleII

---
+++ 119-Pascal'sTriangleII/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace Pascal_sTriangleII
+{
+    internal class Solution
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+
+            List<int> prev = new List<int>() { 1 };
+            for (int i = 0; i < rowIndex; i++)
+            {
+                List<int> current = new();
+                current.Add(1);
+                for (int j = 1; j < prev.Count; j++)
+                {
+                    current.Add(prev[j] + prev[j-1]);
+                }
+                current.Add(1);
+                prev = current;
+            }
+            return prev;
+        }
+    }
+}
