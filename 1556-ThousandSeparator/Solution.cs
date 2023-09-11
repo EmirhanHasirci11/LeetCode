3d8eebbc156ae926dae4a2cb50177986d921283f
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-11 14:54:11 +0300

    1556-ThousandSeparator

---
+++ 1556-ThousandSeparator/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace ThousandSeparator
+{
+    internal class Solution
+    {        
+        public string ThousandSeparator(int n)
+        {
+            if (n.ToString().Length <= 3) return n.ToString();
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            while(n> 0)
+            {
+                sb.Insert(0, n%10);
+                if (count % 3 == 0)
+                    sb.Insert(0, '.');
+                n /= 10;
+                count++;
+            }
+            
+            return sb[0]=='.'?sb.ToString().Substring(1):sb.ToString();
+        }
+    }
+}
