54ced28b42ad2d4f2ac52f3ca67160bd388f3f08
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-13 06:18:34 +0300

    1796-SecondLargestDigitinaString

---
+++ 1796-SecondLargestDigitinaString/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace SecondLargestDigitinaString
+{
+    internal class Solution
+    {        
+        public static int SecondHighest(string s)
+        {
+            int largest = -1, secondLargest = -1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] >= '0' && s[i] <= '9')
+                {
+                    int digit = s[i] - '0';
+                    if (digit > largest)
+                    {
+                        secondLargest = largest;
+                        largest = digit;
+                    }
+                    else if (digit == largest)
+                        continue;
+                    else if (digit > secondLargest)
+                        secondLargest = digit;
+
+                }
+            }
+            return secondLargest;
+        }
+    }
+}
