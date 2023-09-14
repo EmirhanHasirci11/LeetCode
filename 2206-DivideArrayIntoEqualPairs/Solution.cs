dd97283e1913aa99d25e0723efb00c709a756d58
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-14 21:12:18 +0300

    2206-DivideArrayIntoEqualPairs

---
+++ 2206-DivideArrayIntoEqualPairs/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace DivideArrayIntoEqualPairs
+{
+    internal class Solution
+    {        
+        public bool DivideArray(int[] nums)
+        {
+            if (nums.Length % 2 != 0) return false;
+            Dictionary<int, int> map = new();
+            foreach(int num in nums)
+            {
+                if (!map.TryAdd(num, 1))
+                    map[num]++;
+            }
+            foreach(int pairs in map.Values)
+                if (pairs % 2!=0)
+                    return false;
+            return true;
+        }
+    }
+}
