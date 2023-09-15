c6483c26b6db7fb12620f14773f0f6b3110479b0
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-15 19:50:47 +0300

    2418-SortthePeople

---
+++ 2418-SortthePeople/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace SortthePeople
+{
+    internal class Solution
+    {        
+        public string[] SortPeople(string[] names, int[] heights)
+        {
+            string[] result=new string[names.Length];
+
+            Dictionary<int, string> map = new();
+
+            for(int i=0;i<names.Length;i++)
+                map.Add(heights[i], names[i]);
+
+            Array.Sort(heights);
+
+            int index = 0;
+
+            for(int i = heights.Length-1; i >= 0; i--)            
+                result[index++] = map[heights[i]];
+            
+            return result;
+
+
+
+            
+        }
+    }
+}
