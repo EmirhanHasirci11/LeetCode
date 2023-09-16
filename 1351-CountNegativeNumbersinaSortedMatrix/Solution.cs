ea05c4f8c680f624bdada9c22b88e22dce642fcf
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-16 05:10:44 +0300

    1351-CountNegativeNumbersinaSortedMatrix

---
+++ 1351-CountNegativeNumbersinaSortedMatrix/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace CountNegativeNumbersinaSortedMatrix
+{
+    internal class Solution
+    {        
+        public int CountNegatives(int[][] grid)
+        {
+            int countOfNegatives =0;
+            for(int i=0;i<grid.Length;i++)
+            {
+                for(int j = grid[i].Length-1;j>=0;j--)
+                {
+                    if (grid[i][j]<0)
+                        countOfNegatives++;
+                    else
+                        break;
+                }
+            }
+            return countOfNegatives;
+        }
+    }
+}
