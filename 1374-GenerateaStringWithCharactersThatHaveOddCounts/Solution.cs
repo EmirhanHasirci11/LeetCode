574d68cc07c7c2a5819114970570c6549cc7cb03
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-17 07:36:57 +0300

    1374-GenerateaStringWithCharactersThatHaveOddCounts

---
+++ 1374-GenerateaStringWithCharactersThatHaveOddCounts/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace GenerateaStringWithCharactersThatHaveOddCounts
+{
+    internal class Solution
+    {        
+        public string GenerateTheString(int n)
+        {           
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+                sb.Append('a');
+            return n%2!=0? sb.ToString() : sb.ToString().Substring(0,n-1)+'b';
+            
+                
+
+        }
+
+    }
+}
