ee49ad24d2dc1b22d363ebbbadf7e0ff46fbac0e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-18 12:34:07 +0300

    78-Subsets

---
+++ 78-Subsets/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace Subsets
+{
+    internal class Solution
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            backtrack(result, new List<int>(), nums, 0);
+            return result;
+        }
+
+        void backtrack(List<IList<int>> result, List<int> sub, int[] nums, int start)
+        {
+            result.Add(new List<int>(sub));
+            for (int i = start; i < nums.Length; i++)
+            {
+                sub.Add(nums[i]);
+                backtrack(result, sub, nums, i + 1);
+                sub.RemoveAt(sub.Count - 1);
+            }
+        }
+    }
+}
