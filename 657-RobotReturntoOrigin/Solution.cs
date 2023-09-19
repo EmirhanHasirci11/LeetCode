71b68e19c1ae2dcaef3860e201dc4d43d2ddcfeb
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-19 17:38:01 +0300

    657-RobotReturntoOrigin

---
+++ 657-RobotReturntoOrigin/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace RobotReturntoOrigin
+{
+    internal class Solution
+    {        
+        public bool JudgeCircle(string moves)
+        {
+            int x=0;
+            int y=0;
+            foreach(char c in moves)
+            {
+                x += c == 'L' ? -1 : c == 'R' ? +1 : 0;
+                y += c == 'D' ? -1 : c == 'U' ? +1 : 0;
+            }
+            return y==0 && x==0;
+        }
+    }
+}
