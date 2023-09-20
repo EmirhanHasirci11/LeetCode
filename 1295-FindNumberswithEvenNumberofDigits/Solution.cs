ba33dddda0358545bf910c4e4c6624e92559b7d8
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-20 23:07:07 +0300

    1295-FindNumberswithEvenNumberofDigits

---
+++ 1295-FindNumberswithEvenNumberofDigits/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace FindNumberswithEvenNumberofDigits
+{
+    internal class Solution
+    {        
+        public int FindNumbers(int[] nums)
+        {
+            int countOfEven = 0;
+            foreach(int num in nums)
+            {
+                int temp=num;
+                int countTemp = 0;
+                while(temp != 0)
+                {
+                    temp /= 10;
+                    countTemp++;
+                }
+                if (countTemp % 2 == 0)
+                    countOfEven++;
+            }
+            return countOfEven;
+        }
+    }
+}
