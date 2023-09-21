8fcff155e90c730cb808717ad87315e6ee293ce0
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-21 19:27:12 +0300

    1832-CheckiftheSentenceIsPangram

---
+++ 1832-CheckiftheSentenceIsPangram/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace CheckiftheSentenceIsPangram
+{
+    internal class Solution
+    {        
+        public bool CheckIfPangram(string sentence)
+        {
+            HashSet<char> hs = new(sentence);
+            for(int i = 'a'; i <= 'z'; i++)
+            {
+                if(!hs.Contains((char)i))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
