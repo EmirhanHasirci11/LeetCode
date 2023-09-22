e152eaa33f834a82a2211946591b1a2d9a2fd19e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-22 19:47:02 +0300

    1128-NumberofEquivalentDominoPairs

---
+++ 1128-NumberofEquivalentDominoPairs/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace NumberofEquivalentDominoPairs
+{
+    internal class Solution
+    {        
+        public int NumEquivDominoPairs(int[][] dominoes)
+        {
+            Dictionary<int, int> map = new();
+            int count = 0;
+            foreach (int[] d in dominoes)
+            {
+                int key = Math.Min(d[0], d[1]) * 10 + Math.Max(d[0], d[1]);
+                if (!map.TryAdd(key, 1))
+                {
+                    count += map[key];
+                    map[key]++;
+                }               
+            }
+
+            return count;
+        }
+    }
+}
