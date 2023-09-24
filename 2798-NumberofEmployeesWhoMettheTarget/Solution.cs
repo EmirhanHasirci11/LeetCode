9ec773cefb3b93e2f8ae6f4ed2a85a8300377dbb
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-24 21:52:50 +0300

    2798-NumberofEmployeesWhoMettheTarget

---
+++ 2798-NumberofEmployeesWhoMettheTarget/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace NumberofEmployeesWhoMettheTarget
+{
+    internal class Solution
+    {
+        public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+        {
+            return hours.ToList().Where(x => x >= target).Count();
+        }
+    }
+}
