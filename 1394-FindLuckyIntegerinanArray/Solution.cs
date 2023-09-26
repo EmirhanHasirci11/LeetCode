3e02082d15181701bcc3ec3251c2d3daf7b4d7be
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-26 18:24:03 +0300

    1394-FindLuckyIntegerinanArray

---
+++ 1394-FindLuckyIntegerinanArray/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace FindLuckyIntegerinanArray
+{
+    internal class Solution
+    {
+        public int FindLucky(int[] arr)
+        {
+            int result = -1;
+            Dictionary<int, int> map = new();
+            foreach (int i in arr)            
+                if (!map.TryAdd(i, 1))
+                    map[i]++;
+            foreach(int key in map.Keys)
+                if (key == map[key]&&key>result)
+                    result= map[key];
+            return result;
+
+            
+        }
+    }
+}
