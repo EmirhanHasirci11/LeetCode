47b687d92f9711822ab6f695f1881e9316af113f
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-26 18:02:49 +0300

    338-CountingBits

---
+++ 338-CountingBits/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace CountingBits
+{
+    internal class Solution
+    {        
+        public int[] CountBits(int n)
+        {
+            int[] result = new int[n + 1];
+            for(int i = 0; i <= n; i++)
+            {
+                string temp = Convert.ToString(i, 2);
+                result[i] = temp.Count(x=>x=='1');
+            }
+            return result;
+        }
+    }
+}
