b37d72eac311242eca637d5facc9bd3e0b28eeea
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-27 14:12:08 +0300

    2485-FindthePivotInteger

---
+++ 2485-FindthePivotInteger/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace FindthePivotInteger
+{
+    internal class Solution
+    {
+        public int PivotInteger(int n)
+        {
+            int totalSum = n * (n + 1) / 2;
+            int leftSum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                leftSum += i;
+                if (totalSum - leftSum + i == leftSum)
+                    return i;
+            }
+            return -1;
+
+        }
+    }
+}
