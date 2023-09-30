f0b0c204ffc51e7bc15172ad0b4ac9ba2b5c22de
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-09-30 12:09:54 +0300

    2427-NumberofCommonFactors

---
+++ 2427-NumberofCommonFactors/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace NumberofCommonFactors
+{
+    internal class Solution
+    {        
+        public int CommonFactors(int a, int b)
+        {
+            int countOfCommonFactors = 1;
+            int min = Math.Min(a, b);
+            for (int i = 2; i <= min; i++)
+                if (a % i == 0 && b % i == 0)
+                    countOfCommonFactors++;
+
+            return countOfCommonFactors;
+                        
+
+        }
+
+    }
+}
