8efd652106e872b26042589f7d2ffb971b1d2b64
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-01 16:52:52 +0300

    999-AvailableCapturesforRook

---
+++ 999-AvailableCapturesforRook/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace RowWithMaximumOnes
+{
+    internal class Solution
+    {        
+        public int[] RowAndMaximumOnes(int[][] mat)
+        {
+            int minIndex = 0;
+            int maximumOnesCount = 0;
+            for(int i=0;i<mat.Length;i++)
+            {
+                int tempCount = 0;                
+                for (int j =0; j < mat[i].Length;j++)                
+                    if (mat[i][j]==1)
+                        tempCount++;
+                
+                if(tempCount > maximumOnesCount) 
+                {
+                    minIndex= i;
+                    maximumOnesCount = tempCount;
+                }
+            }
+            return new[] {minIndex,maximumOnesCount};
+        }
+    }
+}
