3f61b3ced5f0b183edcf603e3dd601e200b0a78e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-02 23:49:41 +0300

    202-HappyNumber

---
+++ 202-HappyNumber/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace HappyNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public static bool IsHappy(int n)
+        {
+            int valueTimesValue = 0;
+            int sum = 0;
+            HashSet<int> set = new();
+            while(n > 1)
+            {
+                while (n != 0)
+                {
+                    valueTimesValue = n % 10;
+                    sum += valueTimesValue*valueTimesValue;
+                    n /= 10;
+                }
+                if (set.Contains(sum))
+                    return false;
+                set.Add(sum);
+                n = sum;
+                sum = 0;
+            }
+            return true;
+        }
+    }
+}
