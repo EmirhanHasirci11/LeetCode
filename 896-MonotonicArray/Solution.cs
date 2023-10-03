774a3028018d41bef4226fc9fd56009b2206cec0
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-03 20:57:57 +0300

    896-MonotonicArray

---
+++ 896-MonotonicArray/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace MonotonicArray
+{
+    internal class Solution
+    {        
+        public bool IsMonotonic(int[] nums)
+        {
+            bool greater = false;
+            bool lower = false;
+            for(int i = 0; i < nums.Length-1; i++)
+            {
+                int substract = nums[i+1] - nums[i];
+                if (substract > 0)
+                    greater = true;
+                else if(substract<0)
+                    lower = true;
+                if (lower && greater)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
