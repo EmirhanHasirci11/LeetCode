95ff69ef353c84ad89d176e7ea0cab8a6d326bdc
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-04 14:23:15 +0300

    1952-ThreeDivisors

---
+++ 1952-ThreeDivisors/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace ThreeDivisors
+{
+    internal class Solution
+    {        
+        public bool IsThree(int n)
+        {
+            int countOfDivisors = 0;
+            for(int i = 1; i <= n / 2; i++)            
+                if(n%i==0)
+                    countOfDivisors++;
+            
+            return countOfDivisors + 1==3?true:false;
+        }
+    }
+}
