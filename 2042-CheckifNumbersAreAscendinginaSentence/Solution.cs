1465f355a3837a5a039b82bba6b5d4deb1967104
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-05 23:13:53 +0300

    2042-CheckifNumbersAreAscendinginaSentence

---
+++ 2042-CheckifNumbersAreAscendinginaSentence/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace CheckifNumbersAreAscendinginaSentence
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public bool AreNumbersAscending(string s)
+        {
+            string[] arr = s.Split();
+            int prev = -1;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int num = 0;
+                if (int.TryParse(arr[i], out num))
+                {
+                    if (num <= prev)
+                        return false;
+
+                    prev = num;
+                }
+            }
+
+            return true;
+
+        }
+    }
+}
