02eb585b3c28c633c3c5df375ddd900c225f313a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-06 21:51:50 +0300

    2124-CheckifAllA'sAppearsBeforeAllBs

---
+++ 2124-CheckifAllA'sAppearsBeforeAllBs/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace CheckifAllA_sAppearsBeforeAllBs
+{
+    internal class Solution
+    {        
+        public bool CheckString(string s)
+        {
+            bool flag = false;
+            foreach(char c in s)
+            {
+                if (c == 'b')
+                    flag=true;
+                if (flag && c == 'a')
+                    return false;
+            }
+            return true;
+        }
+    }
+}
