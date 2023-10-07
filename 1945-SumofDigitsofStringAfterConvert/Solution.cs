52efd470c921d2502c6fdd8b809c1e6c12d75306
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-07 11:56:07 +0300

    1945-SumofDigitsofStringAfterConvert

---
+++ 1945-SumofDigitsofStringAfterConvert/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace SumofDigitsofStringAfterConvert
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            GetLucky("iiii",1);
+        }
+        public static int GetLucky(string s, int k)
+        {
+            string res = string.Empty;
+            foreach(char c in s)            
+                res += c - 'a' + 1;
+            
+            while (k > 0)
+            {
+                int sumOfDigits = 0;
+                for(int i=0;i<res.Length;i++)
+                {
+                    sumOfDigits += (int)char.GetNumericValue(res[i]);
+                }
+                res = sumOfDigits.ToString();
+                k--;
+                
+            }
+            return Convert.ToInt32(res);
+
+        }
+    }
+}
