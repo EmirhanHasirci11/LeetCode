8a8d03142110f6e974877d20c7d864f8d1043dfb
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-07 11:22:29 +0300

    1365-HowManyNumbersAreSmallerThantheCurrentNumber

---
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace HowManyNumbersAreSmallerThantheCurrentNumber
+{
+    internal class Solution
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            int j = 0;
+            while (j < nums.Length)
+            {
+                int countOfSmallerNumbers = 0;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] < nums[j])
+                        countOfSmallerNumbers++;
+
+                }
+                result[j++]=countOfSmallerNumbers;
+            }
+            return result;
+
+        }
+    }
+}
