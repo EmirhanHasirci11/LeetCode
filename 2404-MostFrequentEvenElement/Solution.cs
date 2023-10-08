a9234d5dd458ef9d7804ffe522b7e032f27f14c2
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-08 18:42:52 +0300

    2404-MostFrequentEvenElement

---
+++ 2404-MostFrequentEvenElement/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace MostFrequentEvenElement
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            MostFrequentEven(new int[] { 0, 1, 2, 2, 4, 4, 1 });
+        }
+        public static int MostFrequentEven(int[] nums)
+        {
+            int evenKey = -1;
+            int frequency = 0;
+            Dictionary<int, int> map = new();
+            foreach (int val in nums)
+            {
+                if (val % 2 == 0)
+                    if (!map.TryAdd(val, 1))
+                        map[val]++;
+            }
+            foreach(int key in map.Keys)
+            {
+                if (map[key] > frequency)
+                {
+                    frequency = map[key];
+                    evenKey= key;
+                }
+               else if (map[key] == frequency && key < evenKey)
+                    evenKey= key;
+                
+            }
+            return evenKey;
+        }
+    }
+}
