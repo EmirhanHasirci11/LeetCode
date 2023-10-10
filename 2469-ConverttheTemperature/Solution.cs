e7723d576d7488f54afec018cfd65817965582c2
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-10 21:21:11 +0300

    2469-ConverttheTemperature

---
+++ 2469-ConverttheTemperature/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace ConverttheTemperature
+{
+    internal class Solution
+    {        
+        public double[] ConvertTemperature(double celsius)
+        {
+            return new double[] { celsius+ 273.15,celsius * 1.80 + 32.00 };
+        }
+    }
+}
