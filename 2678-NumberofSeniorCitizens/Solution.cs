260ef7270bdc6c109fd863887ee58594070591f1
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-12 19:16:35 +0300

    2678-NumberofSeniorCitizens

---
+++ 2678-NumberofSeniorCitizens/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace NumberofSeniorCitizens
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            CountSeniors(new string[] { "7868190130M7522", "5303914400F9211", "9273338290F4010" });
+        }
+        public static int CountSeniors(string[] details)
+        {
+            int countOfSeniorCitizen = 0;
+            foreach (string str in details)
+                if (Convert.ToInt32(str.Substring(11, 2)) > 60)
+                    countOfSeniorCitizen++;
+            return countOfSeniorCitizen;
+        }
+    }
+}
