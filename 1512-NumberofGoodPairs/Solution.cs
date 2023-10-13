44489b33e04fcc5361d8e13c211629697f194add
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-13 10:56:20 +0300

    1512-NumberofGoodPairs

---
+++ 1512-NumberofGoodPairs/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace NumberofGoodPairs
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            NumIdenticalPairs(new int[] {1, 1, 1, 1});
+        }
+        public static int NumIdenticalPairs(int[] nums)
+            {
+                int countOfPairs = 0;
+                for (int i = 0; i < nums.Length; i++)
+                    for (int j = i + 1; j < nums.Length; j++)
+                        if (nums[j] == nums[i] && i < j)
+                            countOfPairs++;
+                return countOfPairs;
+
+
+        }
+    }
+}
