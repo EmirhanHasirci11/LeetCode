c5afe89900f1f9508a743f6507032c3249091424
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-15 11:43:37 +0300

    2016-MaximumDifferenceBetweenIncreasingElements

---
+++ 2016-MaximumDifferenceBetweenIncreasingElements/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace MaximumDifferenceBetweenIncreasingElements
+{
+    internal class Solution
+    {        
+        public int MaximumDifference(int[] nums)
+        {
+            int maximumDifferecenBetweenNumbers = -1;
+            for(int i = 0; i < nums.Length - 1; i++)
+            {
+                for(int j = i + 1; j < nums.Length;j++)
+                {
+                    if (nums[i] < nums[j])
+                        maximumDifferecenBetweenNumbers = nums[j] - nums[i] > maximumDifferecenBetweenNumbers ? nums[j] - nums[i] : maximumDifferecenBetweenNumbers;
+
+                }
+            } 
+                
+                
+            
+            return maximumDifferecenBetweenNumbers;
+        }
+    }
+}
