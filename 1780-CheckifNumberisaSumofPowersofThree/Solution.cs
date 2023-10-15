5ce2b8e8eaa6180d6e7aa8f94dd32a6a486f1026
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-15 12:05:12 +0300

    1780-CheckifNumberisaSumofPowersofThree

---
+++ 1780-CheckifNumberisaSumofPowersofThree/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace CheckifNumberisaSumofPowersofThree
+{
+    internal class Solution
+    {        
+        public bool CheckPowersOfThree(int n)
+        {
+            while(n> 0) 
+            {
+                if (n % 3 == 2)
+                    return false;
+                else
+                    n /= 3;
+            }
+            return true;
+        }
+    }
+}
