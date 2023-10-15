0ba7edbbc834b4afd2d9148554180e2560cdf4c9
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-15 11:31:32 +0300

    977-SquaresofaSortedArray

---
+++ 977-SquaresofaSortedArray/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace SquaresofaSortedArray
+{
+    internal class Solution
+    {        
+        public int[] SortedSquares(int[] nums)
+        {
+            int lengthOfNums = nums.Length;
+            int[] result = new int[lengthOfNums];
+            int left = 0;
+            int right = lengthOfNums - 1;
+            for(int i= lengthOfNums - 1; i >= 0; i--)
+            {
+                if (Math.Abs(nums[left]) > Math.Abs(nums[right]))
+                {
+                    result[i] = nums[left] * nums[left];
+                    left++;
+                }
+                else
+                {
+                    result[i] = nums[right] * nums[right];
+                    right--;
+                }
+            }
+            return result;
+
+        }
+    }
+}
