547d50fc2e9bb33856df056785ac07d0b2c87518
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-16 18:45:02 +0300

    219-ContainsDuplicateII

---
+++ 219-ContainsDuplicateII/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace ContainsDuplicateII
+{
+    internal class Solution
+    {        
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> map = new();
+            for(int i=0;i<nums.Length;i++)
+            {
+                if (map.ContainsKey(nums[i]) && Math.Abs(map[nums[i]] - i) <= k)
+                    return true;
+                map[nums[i]] = i;
+            }
+            return false;
+        }
+    }
+}
