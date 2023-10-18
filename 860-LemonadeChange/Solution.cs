a6f5f93e100efc3d6b758aa7dd4f1be152416447
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-18 12:51:27 +0300

    860-LemonadeChange

---
+++ 860-LemonadeChange/Solution.cs
@@ -0,0 +1,49 @@
+﻿namespace LemonadeChange
+{
+    internal class Solution
+    {
+        public bool LemonadeChange(int[] bills)
+        {
+            Stack<int> bills10 = new();
+            Stack<int> bills5 = new();
+            foreach(int bill in bills)
+            {
+                if(bill==5)
+                    bills5.Push(bill);
+                else if (bill == 10)
+                {
+                    bills10.Push(bill);
+                    if (bills5.Count <= 0)
+                        return false;
+                    else
+                        bills5.Pop();
+                }
+                else if (bill == 20)
+                {
+                    if (bills10.Count > 0)
+                    {
+                        bills10.Pop();
+                        if (bills5.Count <= 0)
+                            return false;
+                        bills5.Pop();
+                    }
+                    else if (bills5.Count > 0)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (bills5.Count > 0)
+                            {
+                                return false;
+                            }
+                            bills5.Pop();
+                        }
+                    }
+                    else
+                        return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
