a3e632fd2425a6345047721eba097aad7a1ac3de
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-19 23:40:44 +0300

    2455-AverageValueofEvenNumbersThatAreDivisiblebyThree

---
+++ 2455-AverageValueofEvenNumbersThatAreDivisiblebyThree/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace AverageValueofEvenNumbersThatAreDivisiblebyThree
+{
+    internal class Solution
+    {
+        
+        public int AverageValue(int[] nums)
+        {
+            int sum = 0;
+            int count = 0;
+            foreach(int val in nums)            
+                if (val % 6 == 0)
+                {
+                    sum += val;
+                    count++;
+                }                           
+            return sum!=0? sum / count:0;
+        }
+    }
+}
