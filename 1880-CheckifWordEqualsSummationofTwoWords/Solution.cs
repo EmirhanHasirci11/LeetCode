25ccdac1da1deaabdb6fa414d6c398ad577d0e5e
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-21 13:43:32 +0300

    1880-CheckifWordEqualsSummationofTwoWords

---
+++ 1880-CheckifWordEqualsSummationofTwoWords/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace CheckifWordEqualsSummationofTwoWords
+{
+    internal class Solution
+    {
+        
+        public bool IsSumEqual(string firstWord, string secondWord, string targetWord)
+        {
+            int first=convertInt(firstWord);
+            int second=convertInt(secondWord);
+            int third=convertInt(targetWord);
+            return first+second==third?true:false;
+        }
+        private int convertInt(string word)
+        {
+            int res = 0;
+            for(int i=0;i<word.Length;i++)            
+                res = res * 10 + (word[i] - 'a');
+            
+            return res;
+        }
+    }
+}
