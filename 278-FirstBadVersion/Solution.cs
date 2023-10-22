4aa0617899558bcedb71b513e6a233a5e5fc3a18
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-22 13:08:23 +0300

    278-FirstBadVersion

---
+++ 278-FirstBadVersion/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace FirstBadVersion
+{
+    internal class Solution
+    {
+        /* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); in leetcode */
+        public int FirstBadVersion(int n)
+        {
+            int left = 0;
+            int right = n;
+            while (left <= right) 
+            {
+                int middle = left+(right-left)/2;
+                if (IsBadVersion(middle))
+                    right = middle - 1;
+                else
+                    left = middle + 1;
+            }
+            return left;
+        }
+    }
+}
