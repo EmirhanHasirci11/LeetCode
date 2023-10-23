d278efc46ed8ffedbf4cefd36873fa859a84a248
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-10-23 13:08:23 +0300

    2903-FindIndicesWithIndexandValueDifferenceI

---
+++ 2903-FindIndicesWithIndexandValueDifferenceI/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace FindIndicesWithIndexandValueDifferenceI
+{
+    internal class Solution
+    {        
+        public int[] FindIndices(int[] nums, int indexDifference, int valueDifference)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = 0; j < nums.Length; j++)
+                    if (Math.Abs(i - j) >= indexDifference && Math.Abs(nums[i] - nums[j]) >= valueDifference)
+                        return new int[] { i, j };
+            return new int[] { -1, -1 };
+        }
+    }
+}
