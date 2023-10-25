e962cce9e15248aef0a8f4f959cb70ad01902a7e
Emirhan Hasırcı <emir_han886@outlook.com>
2023-10-25 22:32:49 +0300

    1523-CountOddNumbersinanIntervalRange

---
+++ 1523-CountOddNumbersinanIntervalRange/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace CountOddNumbersinanIntervalRange
+{
+    internal class Solution
+    {       
+        public int CountOdds(int low, int high)
+        {
+            return (high + 1) / 2 - low / 2;
+        }
+    }
+}
