1caadbf0f5752609fa9cbd83c0378e687a264c1a
Emirhan Hasırcı <emir_han886@outlook.com>
2023-10-27 10:59:52 +0300

    2341-MaximumNumberofPairsinArray

---
+++ 2341-MaximumNumberofPairsinArray/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace MaximumNumberofPairsinArray
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public int[] NumberOfPairs(int[] nums)
+        {
+            int countOfPairs = 0;
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                if (nums[i] == -1)
+                    continue;
+                for(int j = i + 1; j < nums.Length; j++)
+                {
+                    if ((nums[i] == nums[j]) && (nums[i] != 0 && nums[j]!=0))
+                    {
+                        countOfPairs++;
+                        nums[i] = -1;
+                        nums[j] = -1;
+                        break;
+                    }
+                }
+            }
+            int countOfLeftovers = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i]!=-1)
+                    countOfLeftovers++;
+            }
+            return new int[] {countOfPairs,countOfLeftovers};
+        }
+    }
+}
