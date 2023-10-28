305e7efee951b18bf6f6b09e5105ff7c58fbbc5f
Emirhan Hasırcı <emir_han886@outlook.com>
2023-10-28 20:23:06 +0300

    225-ImplementStackusingQueues

---
+++ 225-ImplementStackusingQueues/MyStack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace ImplementStackusingQueues
+{
+    internal class MyStack
+    {
+        private Queue<int> q;
+        public MyStack()
+        {
+            q = new Queue<int>();
+        }
+
+        public void Push(int x)
+        {
+            q.Enqueue(x);
+            for (int i = 0; i < q.Count - 1; i++)
+                q.Enqueue(q.Dequeue());
+        }
+
+        public int Pop()
+        {
+            return q.Dequeue();
+        }
+
+        public int Top()
+        {
+            return q.Peek();
+        }
+
+        public bool Empty()
+        {
+            return q.Count== 0;
+        }
+    }
+}
