f275350c0948c006f50fb79aa81523d5fca1097e
Emirhan Hasırcı <emir_han886@outlook.com>
2023-10-29 14:57:02 +0300

    14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LongestCommonPrefix
+{
+    internal class Program
+    {        
+        public string LongestCommonPrefix(string[] strs)
+        {
+            Array.Sort(strs);
+            string first = strs[0];
+            string last = strs[strs.Length-1];
+            int index = 0;
+            while(index<first.Length && index<last.Length) 
+            {
+                if (first[index] == last[index])
+                    index++;
+                else
+                    break;
+            }
+            return first.Substring(0, index);
+        }
+    }
+}
