7dc805e70008236d9accec037c1804102f16584b
Emirhan Hasırcı <emir_han886@outlook.com>
2023-10-30 18:40:26 +0300

    566-ReshapetheMatrix

---
+++ 566-ReshapetheMatrix/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace ReshapetheMatrix
+{
+    internal class Solution
+    {        
+        public int[][] MatrixReshape(int[][] mat, int r, int c)
+        {
+            int rows = mat.Length;
+            int cols = mat[0].Length;
+            if (cols * rows != r * c) return mat;
+
+            int[][] output = new int[r][];           
+            int resultRow = 0;
+            int resultCol = 0;
+            for (int i = 0; i < r; i++)
+            {
+                output[i] = new int[c];
+            }
+            for (int i = 0; i < rows; i++)
+            {
+              
+                for(int j = 0; j < cols; j++)
+                {
+                    output[resultRow][resultCol] = mat[i][j];
+                    resultCol++;
+                    if(resultCol == c) 
+                    {
+                        resultCol= 0;
+                        resultRow++;
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
