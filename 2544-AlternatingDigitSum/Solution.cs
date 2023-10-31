56ed1e8c91b29932dcb1ebc99080beacaaf19439
Emirhan Hasırcı <emir_han886@outlook.com>
2023-10-31 23:37:21 +0300

    2544-AlternatingDigitSum

---
+++ 2544-AlternatingDigitSum/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace AlternatingDigitSum
+{
+    internal class Solution
+    {       
+        public int AlternateDigitSum(int n)
+        {
+            string s = "" + n;
+
+            int total = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0) total += s[i] - '0';
+                else total -= s[i] - '0';
+            }
+
+            return total;
+        }
+    }
+}
