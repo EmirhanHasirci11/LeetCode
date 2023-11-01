0d234de16d2b4af3556a067afbdaf98c1963f6b3
Emirhan Hasırcı <emir_han886@outlook.com>
2023-11-01 23:02:49 +0300

    2264-LargestThree-Same-DigitNumberinString

---
+++ 2264-LargestThree-Same-DigitNumberinString/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LargesTthree_Same_DigitNumberinString
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LargestGoodInteger("6777133339"));
+        }
+        public static string LargestGoodInteger(string num)
+        {
+            char c = num[0];
+            int countOfSubstring = 1;
+            int largestGoodInteger = 0;
+            for(int i=1;i<num.Length;i++)
+            {
+                if (c == num[i])
+                {
+                    countOfSubstring++;
+                }
+                else
+                {
+                    countOfSubstring=1;
+                }
+                if (countOfSubstring == 3)
+                {
+                    largestGoodInteger = Math.Max(largestGoodInteger, num[i]);
+                }
+                c= num[i];
+            }
+            return new string((char)largestGoodInteger, 3);
+        }
+    }
+}
