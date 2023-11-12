af257c9786b05b0d732ef41528c591c3cd004670
Emirhan Hasırcı <emir_han886@outlook.com>
2023-11-12 03:17:32 +0300

    2586-CounttheNumberofVowelStringsinRange

---
+++ 2586-CounttheNumberofVowelStringsinRange/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace CounttheNumberofVowelStringsinRange
+{
+    internal class Solution
+    {        
+        public int VowelStrings(string[] words, int left, int right)
+        {
+            char[] vowels = new[] { 'a', 'e', 'i', 'o','u' };
+            int countOfVowelString = 0;
+            while (left <= right)
+            {
+                if (vowels.Contains(words[left][0]) && vowels.Contains(words[left][words[left].Length - 1]))
+                    countOfVowelString++;
+                left++;
+            }
+            return countOfVowelString;
+        }
+    }
+}
