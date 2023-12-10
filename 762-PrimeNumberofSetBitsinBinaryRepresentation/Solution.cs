287032d6df2dc5d66b73a9b3b93b2fcdacff2745
Emirhan Hasırcı <emir_han886@outlook.com>
2023-12-10 15:44:41 +0300

    762-PrimeNumberofSetBitsinBinaryRepresentation

---
+++ 762-PrimeNumberofSetBitsinBinaryRepresentation/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace PrimeNumberofSetBitsinBinaryRepresentation
+{
+    internal class Solution
+    {       
+        public int CountPrimeSetBits(int left, int right)
+        {
+            StringBuilder sb = new();
+            int count = 0;
+            while(left<=right)
+            {
+                int countOfSetBit = 0;
+                sb.Append(Convert.ToString(left++,2));
+                foreach (char c in sb.ToString())
+                    countOfSetBit += c == '1' ? 1 : 0;
+                count += isPrime(countOfSetBit) ? 1 : 0;
+                sb.Clear();
+            }
+            return count;
+        }
+        private bool isPrime(int num)
+        {
+            if(num==2) return true;
+            if (num <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
