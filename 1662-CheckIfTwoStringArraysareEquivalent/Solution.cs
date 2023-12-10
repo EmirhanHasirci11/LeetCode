24361f649b86a92f91a18a991cd9d570646eb5a0
Emirhan Hasırcı <emir_han886@outlook.com>
2023-12-10 15:59:49 +0300

    1662-CheckIfTwoStringArraysareEquivalent

---
+++ 1662-CheckIfTwoStringArraysareEquivalent/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace CheckIfTwoStringArraysareEquivalent
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            StringBuilder sb1= new ();
+            StringBuilder sb2= new ();
+            word1.ToList().ForEach(c=>sb1.Append(c));
+            word2.ToList().ForEach(c=>sb2.Append(c));
+            return sb1.ToString().Equals(sb2.ToString());
+        }
+    }
+}
