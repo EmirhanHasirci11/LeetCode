2c69aaec9be06dab009d158a275c29e8621e4d63
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-01 11:48:17 +0300

    455-AssignCookies

---
+++ 455-AssignCookies/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace AssignCookies
+{
+    internal class Solution
+    {     
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            if(s.Length== 0) return 0;
+            Array.Sort(s);
+            Array.Sort(g);
+            int countOfHappyChild = 0;
+            int lengthOfChild = g.Length - 1;
+            int lengthOfCookies = s.Length - 1;
+            while(lengthOfCookies >= 0 && lengthOfChild>=0)
+            {
+                if (s[lengthOfCookies] >= g[lengthOfChild]) 
+                {
+                    countOfHappyChild++;
+                    lengthOfCookies--;
+                    lengthOfChild--;
+                }
+                else
+                {
+                    lengthOfChild--;
+                }
+
+            }
+            return countOfHappyChild;
+        }
+    }
+}
