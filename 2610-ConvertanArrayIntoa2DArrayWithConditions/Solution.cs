7642de2542e84c46b118451b1812a513b4e48b1f
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-02 23:22:42 +0300

    2610-ConvertanArrayIntoa2DArrayWithConditions

---
+++ 2610-ConvertanArrayIntoa2DArrayWithConditions/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace ConvertanArrayIntoa2DArrayWithConditions
+{
+    internal class Solution
+    {       
+        public IList<IList<int>> FindMatrix(int[] nums)
+        {
+            List<int> ints=nums.ToList();
+            List<IList<int>> res = new();            
+            HashSet<int> visited = new();            
+            while (ints.Count!=0) 
+            {
+                List<int> temp = new ();
+                for(int i=0;i<ints.Count;i++)
+                {
+                    if (!visited.Contains(ints[i]))
+                    {
+                        visited.Add(ints[i]);
+                        temp.Add(ints[i]);
+                        ints.RemoveAt(i);
+                        i--;
+                    }
+                }
+                res.Add(temp);
+                visited.Clear();
+            }
+            return res;
+
+        }
+    }
+}
