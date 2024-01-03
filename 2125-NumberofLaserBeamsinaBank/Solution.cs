9bde0cffcc75f9e3e818966b4db4cfab2fe9b198
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-03 23:25:44 +0300

    2125-NumberofLaserBeamsinaBank

---
+++ 2125-NumberofLaserBeamsinaBank/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace NumberofLaserBeamsinaBank
+{
+    internal class Solution
+    {        
+        public int NumberOfBeams(string[] bank)
+        {
+            int lastLaserRow = 0;
+            int countOfLaserBeam = 0;
+            foreach(string bankName in bank)
+            {
+                int curRowCount = countOfBeams(bankName);
+                if (curRowCount == 0)
+                    continue;
+
+                countOfLaserBeam += curRowCount * lastLaserRow;
+                lastLaserRow = curRowCount;
+            }
+            return countOfLaserBeam;
+                
+        }
+        public int countOfBeams(string row)
+        {
+            int countOfCurrentRowBeam = 0;
+            foreach(char c in row)
+            {
+                countOfCurrentRowBeam += c - '0';
+            }
+            return countOfCurrentRowBeam;
+        }
+    }
+}
