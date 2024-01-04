d782d7f2b2b4a4e4bfd0e76f3f2dab7338e99b49
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-05 00:04:38 +0300

    2870-MinimumNumberofOperationstoMakeArrayEmpty

---
+++ 2870-MinimumNumberofOperationstoMakeArrayEmpty/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace MinimumNumberofOperationstoMakeArrayEmpty
+{
+    internal class Solution
+    {
+        public int MinOperations(int[] nums)
+        {
+            Dictionary<int, int> map = new();
+            foreach (int num in nums)            
+                if (!map.TryAdd(num, 1))
+                    map[num]++;
+            
+            int totalMinimumOperationCount = 0;
+            foreach(int key in map.Keys)
+            {
+                int valueOfKey = map[key];
+
+                if (valueOfKey == 1)
+                    return -1;
+                totalMinimumOperationCount+=valueOfKey/3;
+                if(valueOfKey%3!=0)
+                    totalMinimumOperationCount++;
+            }
+            return totalMinimumOperationCount;
+        }
+    }
+}
