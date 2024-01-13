6144b3a7a4907b95e57f4594da4a0bf73e98841f
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-13 15:40:16 +0300

    1347-MinimumNumberofStepstoMakeTwoStringsAnagram

---
+++ 1347-MinimumNumberofStepstoMakeTwoStringsAnagram/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace MinimumNumberofStepstoMakeTwoStringsAnagram
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int MinSteps(string s, string t)
+        {
+            int[] sA = new int[26];
+            int[] tA = new int[26];
+            for(int i = 0; i < s.Length; i++)
+            {
+                sA[s[i]-'a']++;
+                tA[t[i]-'a']++;
+            }
+            int step = 0;
+            for (int i = 0; i < 26; i++)
+                step += Math.Abs(sA[i] - tA[i]);
+            return step/2;
+        }
+    }
+}
