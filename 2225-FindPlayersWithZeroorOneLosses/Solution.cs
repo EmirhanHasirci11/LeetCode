267d0f7364217361f16b865598d55a2813e0c54a
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-15 15:52:36 +0300

    Update Solution.cs

--- 2225-FindPlayersWithZeroorOneLosses/Solution.cs
+++ 2225-FindPlayersWithZeroorOneLosses/Solution.cs
@@ -4,20 +4,16 @@
     {
         public IList<IList<int>> FindWinners(int[][] matches)
         {
-            
-            var map = new SortedDictionary<int, int>();
-            foreach (int[] match in matches)
-            {
-                map.TryAdd(match[0], map.GetValueOrDefault(match[0], 0));
-                map.TryAdd(match[1], map.GetValueOrDefault(match[1], 0) + 1);
-            };
-                
-            List<IList<int>> list = new() { new List<int>(), new List<int>() };
-            foreach (int player in map.Keys)
-                if (map[player] <= 1)
-                    list[map[player]].Add(player);
-            return list;
-            
+
+            HashSet<int> winners = new(matches.Select(x => x[0]));
+            List<int> losers = new(matches.Select(x => x[1]));
+
+            return new List<IList<int>>
+        {
+            winners.Except(losers).OrderBy(x => x).ToList(),
+            losers.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).OrderBy(x => x).ToList()
+        };
+
 
         }
     }
