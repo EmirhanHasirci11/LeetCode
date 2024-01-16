3dd5c4de8d975e993f7179caeb03bf974240270b
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-16 22:02:27 +0300

    380-InsertDeleteGetRandomO(1)

---
+++ 380-InsertDeleteGetRandomO(1)/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace InsertDeleteGetRandomO_1_
+{
+    public class RandomizedSet
+    {      
+        HashSet<int> set = new();
+        Random rnd = new();
+
+        public bool Insert(int n) => set.Add(n);
+
+        public bool Remove(int n) => set.Remove(n);
+
+        public int GetRandom() => set.ElementAt(rnd.Next(set.Count));
+    }
+
+}
