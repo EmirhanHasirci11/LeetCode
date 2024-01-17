98b683792eb7f8012ae58a6510d03dec0fb4bb0a
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-17 21:09:41 +0300

    2194-CellsinaRangeonanExcelSheet

---
+++ 2194-CellsinaRangeonanExcelSheet/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace CellsinaRangeonanExcelSheet
+{
+    internal class Solution
+    {        
+        public IList<string> CellsInRange(string s)
+        {
+            List<string> list = new ();
+            for(char i = s[0]; i <= s[3]; i++)            
+                for(char j = s[1];j <= s[4];j++)
+                    list.Add (""+i+j);
+            return list;
+            
+        }
+    }
+}
