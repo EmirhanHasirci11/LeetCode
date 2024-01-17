21effe7cce36623bfb6e11d9ce06bee673c0a1c5
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-17 21:34:36 +0300

    283-MoveZeroes

---
+++ 283-MoveZeroes/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace MoveZeroes
+{
+    internal class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int indexOfZero = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)                
+                    indexOfZero++;                
+                else if (indexOfZero > 0)
+                {
+                    int temp = nums[i];
+                    nums[i] = 0;
+                    nums[i - indexOfZero] = temp;
+                }
+            }
+        }
+    }
+}
