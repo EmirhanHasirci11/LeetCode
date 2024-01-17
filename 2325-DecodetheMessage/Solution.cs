02296a3d6cf4e8797e2f2cd3a76f1706ec447b6d
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-17 20:47:11 +0300

    2325-DecodetheMessage

---
+++ 2325-DecodetheMessage/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace DecodetheMessage
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DecodeMessage("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv"));
+        }
+        public static string DecodeMessage(string key, string message)
+        {
+            HashSet<char> keys = new();
+            Dictionary<char, char> decode = new();
+            char alph = 'a';
+            foreach (char c in key.Replace(" ",""))
+            {
+                if (!keys.TryGetValue(c, out char d))
+                {
+                    keys.Add(c);
+                    decode.Add(c, alph++);
+                }
+
+            }
+            StringBuilder sb = new();
+            foreach (char c in message.Replace(" ", ""))
+            {
+                sb.Append(decode[c]);
+            }
+            return sb.ToString();
+        }
+    }
+}
