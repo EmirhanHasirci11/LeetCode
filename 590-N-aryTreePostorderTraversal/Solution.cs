24caed44adab7808b79d6a00720195717ae12eef
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-17 20:56:35 +0300

    590-N-aryTreePostorderTraversal

---
+++ 590-N-aryTreePostorderTraversal/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Linq;
+
+namespace N_aryTreePostorderTraversal
+{
+
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    internal class Solution
+    {
+        public IList<int> Postorder(Node root)
+        {
+            List<int> res = new();
+            Travel(root,res);
+            return res;
+        }
+        public  void Travel(Node node,List<int> nodeList) 
+        {
+            if(node is not null)
+            {
+                foreach(Node child in node.children) 
+                {
+                    Travel(child, nodeList);
+                }
+                nodeList.Add(node.val);
+            }
+        }
+
+    }
+}
