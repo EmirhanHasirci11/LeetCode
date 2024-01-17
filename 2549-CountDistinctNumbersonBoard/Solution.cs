dea5fe029a5ddb324274fbeb8e2847b0a4f88a00
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-17 20:15:19 +0300

    2549-CountDistinctNumbersonBoard

---
+++ 2549-CountDistinctNumbersonBoard/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace CountDistinctNumbersonBoard
+{
+    public class Solution
+    {
+        public int DistinctIntegers(int n)
+        {
+            return Math.Max(n - 1, 1);
+        }
+    }
+}
