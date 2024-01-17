101eca68264028d4c36258228d1e5ebceadc1908
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-17 21:24:14 +0300

    796-RotateString

---
+++ 796-RotateString/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace RotateString
+{
+    internal class Solution
+    {        
+        public bool RotateString(string s, string goal)
+        {
+            StringBuilder sb= new (s);
+            for(int i=0;i<s.Length;i++)
+            {
+                sb.Insert(0, s[s.Length-1]);
+                sb.Remove(sb.Length-1,1);
+                if(sb.ToString().Equals(goal))
+                    return true;
+                s=sb.ToString();
+            }
+            return false;
+        }
+    }
+}
