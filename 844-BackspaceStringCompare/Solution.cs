53bcf04e89de80187edfe6c5f36277aaf6d28588
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-18 11:58:53 +0300

    844-BackspaceStringCompare

---
+++ 844-BackspaceStringCompare/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace BackspaceStringCompare
+{
+    internal class Solution
+    {        
+        public bool BackspaceCompare(string s, string t)
+        {
+            string first = ResultString(s);
+            string second = ResultString(t);
+            return first == second;
+        }
+        public string ResultString(string temp)
+        {
+            StringBuilder sb = new (temp);
+            int index = 0;
+            while (index < sb.Length - 1)
+            {
+                if (sb[index + 1] == '#')
+                {
+                    sb.Remove(index, 2);
+                    index = 0;
+                    continue;
+                }
+                index++;
+            }
+            sb.Replace("#","");
+            
+            return sb.ToString();
+        }
+    }
+}
