ceffb8c3076040e5f3ebb431a9e29ae2041fde68
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-20 20:46:22 +0300

    907-SumofSubarrayMinimums

---
+++ 907-SumofSubarrayMinimums/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace SumofSubarrayMinimums
+{
+    internal class Solution
+    {
+        public int SumSubarrayMins(int[] arr)
+        {
+            long sum = 0;
+            Stack<int> stack = new();
+            for (int i = 0; i <= arr.Length; i++)
+            {
+                while (stack.Count > 0 && arr[stack.Peek()] > (i == arr.Length ? Int32.MinValue : arr[i]))
+                {
+
+                    int mid = stack.Pop();
+                    int left = stack.Count == 0 ? -1 : stack.Peek();
+                    int right = i;
+                    sum += (long)(arr[mid]) * (mid - left) * (right - mid);
+                }
+                stack.Push(i);
+            }
+
+            return (int)(sum % 1000000007);
+        }
+    }
+}
