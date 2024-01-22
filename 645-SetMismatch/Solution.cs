7294dc79f99db72d6905f4e2e79e09f2a6a3db92
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-22 06:08:11 +0300

    645-SetMismatch

---
+++ 645-SetMismatch/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace SetMismatch
+{
+    internal class Solution
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            int duplicate = 0;
+            int misVal = 0;
+            HashSet<int> set = new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (set.TryGetValue(nums[i], out int b))
+                    duplicate = nums[i];
+                set.Add(nums[i]);
+            }
+            for(int i = 1; i <= nums.Length; i++)
+            {
+                if (!set.Contains(i))
+                {
+                    misVal= i;
+                    break;
+                }
+            }
+
+            return new int[] {duplicate,misVal};
+        }
+    }
+}
