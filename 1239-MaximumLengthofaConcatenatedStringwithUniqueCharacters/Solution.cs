353347a8aee20898779f8b5be59af36f3751365b
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-23 13:49:25 +0300

    1239-MaximumLengthofaConcatenatedStringwithUniqueCharacters

---
+++ 1239-MaximumLengthofaConcatenatedStringwithUniqueCharacters/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace MaximumLengthofaConcatenatedStringwithUniqueCharacters
+{
+    internal class Solution
+    {
+        private int res = 0;
+        public int MaxLength(IList<string> arr)
+        {
+            if(arr.Count==0) return 0;
+
+            dfs(arr, "", 0);
+            return res;
+        }
+        private void dfs(IList<string> list, string val, int index)
+        {
+            bool checkUnique = CheckUnique(val);
+            if (checkUnique)
+                res = Math.Max(val.Length, res);
+
+            if (index == list.Count || !checkUnique)
+                return;
+
+            for (int i = index; i < list.Count; i++)
+                dfs(list, val + list[i], i + 1);
+
+
+
+        }
+        private bool CheckUnique(string txt)
+        {
+            HashSet<char> unique = new();
+            foreach (char c in txt)
+            {
+                if (unique.Contains(c))
+                    return false;
+                else
+                    unique.Add(c);
+            }
+            return true;
+        }
+    }
+}
