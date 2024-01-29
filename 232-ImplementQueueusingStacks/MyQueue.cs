344797d163a529500742100975205145e58076b6
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-29 03:39:43 +0300

    232-ImplementQueueusingStacks

---
+++ 232-ImplementQueueusingStacks/MyQueue.cs
@@ -0,0 +1,39 @@
+﻿namespace ImplementQueueusingStacks
+{
+    public class MyQueue
+    {
+        Stack<int> input;
+        Stack<int> output;
+        public MyQueue()
+        {
+            input = new();
+            output=new();
+        }
+
+        public void Push(int x)
+        {
+            input.Push(x);
+        }
+
+        public int Pop()
+        {
+            int current=Peek();
+            output.Pop();
+            return current;
+        }
+
+        public int Peek()
+        {
+            if(output.Count==0)
+                while(input.Count!=0)
+                    output.Push(input.Pop());
+            return output.Peek();
+        }
+
+        public bool Empty()
+        {
+            return input.Count==0&&output.Count==0;
+        }
+    }
+
+}
