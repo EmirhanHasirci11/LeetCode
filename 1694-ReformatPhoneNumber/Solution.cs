923f61b74220bca164ba8e44cd7243e55626b24e
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-31 05:29:10 +0300

    1694-ReformatPhoneNumber

---
+++ 1694-ReformatPhoneNumber/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace ReformatPhoneNumber
+{
+    internal class Solution
+    {        
+        public string ReformatNumber(string number)
+        {
+            StringBuilder sb = new();
+            foreach (char c in number)
+                if (char.IsDigit(c))
+                    sb.Append(c);
+
+            StringBuilder res = new();
+            int idx = 0;
+            while (idx < sb.Length)
+            {
+                int j = (sb.Length - idx == 4) ? idx + 2 : idx + 3;
+
+                while (idx < sb.Length && idx < j)
+                    res.Append(sb[idx++]);
+                if (idx != sb.Length)
+                    res.Append('-');
+            }
+            return res.ToString();
+        }
+    }
+}
