9adcd2be0a7b5939aca4336fcf27e3aed4db1701
Emirhan Hasırcı <emir_han886@outlook.com>
2024-01-31 05:05:05 +0300

    739-DailyTemperatures

---
+++ 739-DailyTemperatures/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyTemperatures
+{
+    internal class Solution
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            Stack<int> stack= new ();
+            int[] result = new int[temperatures.Length];
+            for(int i=0;i<temperatures.Length;i++) 
+            {
+                while (stack.Count > 0 && (temperatures[i] > temperatures[stack.Peek()]))
+                {
+                    int idx = stack.Pop();
+                    result[idx] = i-idx;
+                }
+                stack.Push(i);
+            }
+            return result;
+        }
+    }
+}
