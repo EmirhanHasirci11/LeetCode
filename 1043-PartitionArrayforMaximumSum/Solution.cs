dff6a85acfec3f502534ef138dfae63aa8a7bb7c
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-03 16:58:54 +0300

    1043-PartitionArrayforMaximumSum

---
+++ 1043-PartitionArrayforMaximumSum/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace PartitionArrayforMaximumSum
+{
+    internal class Solution
+    {        
+        public int MaxSumAfterPartitioning(int[] arr, int k)
+        {
+            int N = arr.Length; 
+            int [] dp = new int[N];
+            for (int i = 0; i < N; ++i)
+            {
+                int curMax = 0;
+                for (int j = 1; j <= k && i - j + 1 >= 0; ++j)
+                {
+                    curMax = Math.Max(curMax, arr[i - j + 1]);
+                    dp[i] = Math.Max(dp[i], (i >= j ? dp[i - j] : 0) + curMax * j);
+                }
+            }
+            return dp[N - 1];
+        }
+    }
+}
