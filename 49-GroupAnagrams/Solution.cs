9c915931978862a41459e7a4dd1cca22db662891
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-06 07:51:57 +0300

    49-GroupAnagrams

---
+++ 49-GroupAnagrams/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace GroupAnagrams
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, IList<string>> map = new();
+            foreach (string s in strs)
+            {
+                char[] cArr = s.ToCharArray();
+                Array.Sort(cArr);
+                string sorted = new string(cArr);
+                if (!map.TryAdd(sorted, new List<string>() { s }))
+                    map[sorted].Add(s);
+            }
+            IList<IList<string>> list = map.Values.ToList();
+            return list;
+
+        }
+    }
+}
