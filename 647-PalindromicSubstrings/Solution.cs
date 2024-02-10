7c4e55da315910504c9a58d11776a0048fbfcd30
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-10 03:30:41 +0300

    647-PalindromicSubstrings

---
+++ 647-PalindromicSubstrings/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace PalindromicSubstrings
+{
+    internal class Solution
+    {        
+        public int CountSubstrings(string s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                count += expand(s, i, i);
+                count += expand(s, i, i + 1);
+            }
+            return count;
+        }
+        private int expand(String s, int left, int right)
+        {
+            int n = s.Length, count = 0;
+            while (left >= 0 && right < n && s[left] == s[right])
+            {
+                count++;
+                left--;
+                right++;
+            }
+            return count;
+        }
+    }
+}
