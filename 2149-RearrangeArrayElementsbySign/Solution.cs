c171e92bbcfc22772ba996845e74aa9dd1688a8f
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-14 09:29:11 +0300

    2149-RearrangeArrayElementsbySign

---
+++ 2149-RearrangeArrayElementsbySign/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace RearrangeArrayElementsbySign
+{
+    public class Solution
+    {
+        public int[] RearrangeArray(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+            int index = 0, pi = 0, ni = 1;
+            while (index < nums.Length)
+            {
+                if (nums[index] >0) 
+                {
+                    res[pi] = nums[index++];
+                    pi+=2;
+                }
+                else
+                {
+                    res[ni] = nums[index++];
+                    ni += 2;
+                }
+            }
+            return res;
+        }
+    }
+}
