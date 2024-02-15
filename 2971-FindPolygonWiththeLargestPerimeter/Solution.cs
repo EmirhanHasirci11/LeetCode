770d9a904cd5707856039be36446b5a8be3a398e
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-15 14:54:09 +0300

    2971-FindPolygonWiththeLargestPerimeter

---
+++ 2971-FindPolygonWiththeLargestPerimeter/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace FindPolygonWiththeLargestPerimeter
+{
+    internal class Solution
+    {
+        public long LargestPerimeter(int[] nums)
+        {
+            Array.Sort(nums);
+            long res = -1;
+            long total = 0;
+            foreach (int i in nums)
+            {
+                if (total > i)
+                    res = total + i;
+                total += i;
+            }
+            return res;
+        }
+    }
+}
