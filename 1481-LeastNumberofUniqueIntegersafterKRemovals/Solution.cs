9e93adb070e775a0d7f40be1c418f118297f4b43
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-16 17:20:40 +0300

    1481-LeastNumberofUniqueIntegersafterKRemovals

---
+++ 1481-LeastNumberofUniqueIntegersafterKRemovals/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeastNumberofUniqueIntegersafterKRemovals
+{
+    internal class Solution
+    {
+        public int FindLeastNumOfUniqueInts(int[] arr, int k)
+        {
+            Dictionary<int, int> freq = new();
+
+            foreach (int i in arr)
+                if (!freq.TryAdd(i, 1))
+                    freq[i]++;
+
+            var sortedDict = freq.OrderBy(x => x.Value).ToList();
+            foreach (var kvp in sortedDict)
+            {
+                if (k > 0)
+                {
+                    if (k >= kvp.Value)
+                    {
+                        k -= kvp.Value;
+                        freq.Remove(kvp.Key);
+                    }
+                    else
+                    {
+                        freq[kvp.Key] -= k;
+                        k = 0;
+                    }
+                }
+                else
+                    break;
+
+            }
+
+
+            return freq.Count;
+        }
+    }
+}
