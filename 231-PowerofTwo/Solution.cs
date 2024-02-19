b9ea243a7bfd6223b382af7975932b1cf6d83524
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-19 18:59:25 +0300

    231-PowerofTwo

---
+++ 231-PowerofTwo/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace PowerofTwo
+{
+    internal class Solution
+    {        
+        public bool IsPowerOfTwo(int n)
+        {
+            return (n > 0 && (((1 << 30) % n) == 0));
+        }
+    }
+}
