e67826a2b9967a8fe2e9bec27fc8e7caa884d523
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-21 21:26:40 +0300

    201-BitwiseANDofNumbersRange

---
+++ 201-BitwiseANDofNumbersRange/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace BitwiseANDofNumbersRange
+{
+    internal class Solution
+    {
+        public int RangeBitwiseAnd(int left, int right)
+        {
+            while (right > left) right &= right - 1;
+            return right;
+        }
+    }
+}
