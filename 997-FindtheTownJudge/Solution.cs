6f6c518890dcab2ce7324da550146cd9b717375a
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-22 10:17:49 +0300

    997-FindtheTownJudge

---
+++ 997-FindtheTownJudge/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace FindtheTownJudge
+{
+    internal class Solution
+    {
+        public int FindJudge(int n, int[][] trust)
+        {
+            int[] count = new int[n + 1];
+
+            foreach (int[] t in trust)
+            {
+                --count[t[0]];
+                ++count[t[1]];
+            }
+
+            for (int i = 1; i < n + 1; ++i)
+                if (count[i] == n - 1)
+                    return i;
+
+            return -1;
+
+        }
+    }
+}
