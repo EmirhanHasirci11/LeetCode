b79a39e13a78eb26b93a2a1d31b76246a3cfc585
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-26 19:51:04 +0300

    100-SameTree

---
+++ 100-SameTree/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace SameTree
+{
+    internal class Solution
+    {
+        
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+            
+            if (p == null || q == null || p.val != q.val)
+            {
+                return false;
+            }
+            
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
