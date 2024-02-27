2ccd931ab364a8cd13e6d1aa908995f7a1157da2
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-27 22:46:53 +0300

    543-DiameterofBinaryTree

---
+++ 543-DiameterofBinaryTree/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace DiameterofBinaryTree
+{
+    public class Solution
+    {
+        int max = 0;
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            this.Helper(root);
+            return max;
+        }
+
+        public int Helper(TreeNode root)
+        {
+            if (root == null) return 0;
+            int left = this.Helper(root.left);
+            int right = this.Helper(root.right);
+            max = Math.Max(max, left + right);
+            return Math.Max(left, right) + 1;
+        }
+    }
+}
