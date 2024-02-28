f8fbb136832290ca896d88e06c323af574c9d668
Emirhan Hasırcı <emir_han886@outlook.com>
2024-02-28 21:36:11 +0300

    513-FindBottomLeftTreeValue

---
+++ 513-FindBottomLeftTreeValue/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace FindBottomLeftTreeValue
+{
+    internal class Solution
+    {
+        public int FindBottomLeftValue(TreeNode root)
+        {
+            Queue<TreeNode> queue = new();
+            queue.Enqueue(root);
+            int max = 0;
+            while (queue.Count != 0)
+            {
+                root= queue.Dequeue();
+                if(root.right!=null)
+                    queue.Enqueue(root.right);
+                if (root.left != null)
+                    queue.Enqueue(root.left);
+            }
+            return root.Value;
+        }
+    }
+}
