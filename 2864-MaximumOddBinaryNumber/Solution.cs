58e7e125767a14b0cbbee934649ada038c3ca6f1
Emirhan Hasırcı <emir_han886@outlook.com>
2024-03-01 12:32:24 +0300

    2864-MaximumOddBinaryNumber

---
+++ 2864-MaximumOddBinaryNumber/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace MaximumOddBinaryNumber
+{
+    internal class Solution
+    {
+        public string MaximumOddBinaryNumber(string s)
+        {            
+            int onesCount = 0;
+            for (int i = 0; i < s.Length; i++)            
+                onesCount += s[i] - '0';
+            
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < onesCount - 1; i++)            
+                result.Append("1");
+            
+            for (int i = 0; i < s.Length - onesCount; i++)            
+                result.Append("0");
+            
+            result.Append("1");
+            return result.ToString();
+
+        }
+    }
+}
