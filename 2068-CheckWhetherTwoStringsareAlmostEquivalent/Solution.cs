b16d98e657d5eef7a80ca7bac615774c74de8d09
Emirhan Hasırcı <emir_han886@outlook.com>
2024-03-02 22:20:34 +0300

    2068-CheckWhetherTwoStringsareAlmostEquivalent

---
+++ 2068-CheckWhetherTwoStringsareAlmostEquivalent/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace CheckWhetherTwoStringsareAlmostEquivalent
+{
+    internal class Solution
+    {        
+        public bool CheckAlmostEquivalent(string word1, string word2)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            for (int i = 0; i < word1.Length; i++)
+            {
+                char char1 = word1[i];
+                char char2 = word2[i];
+                map[char1] = map.GetValueOrDefault(char1, 0) + 1;
+                map[char2] = map.GetValueOrDefault(char2, 0) - 1;
+            }
+            foreach (int value in map.Values)
+            {
+                if (value > 3 || value < -3)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
