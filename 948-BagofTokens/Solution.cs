7d94e8ad4422ac5ae3d6cf9bf42b683f4292f829
Emirhan Hasırcı <emir_han886@outlook.com>
2024-03-04 19:06:42 +0300

    948-BagofTokens

---
+++ 948-BagofTokens/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace BagofTokens
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BagOfTokensScore(new int[] {200,100},150));
+        }
+        public static int BagOfTokensScore(int[] tokens, int power)
+        {
+            Array.Sort(tokens);
+            int left =0,right= tokens.Length-1;
+            int res = 0, score = 0 ;
+            while(left <= right)
+            {
+                if (power >= tokens[left])
+                {
+                    power -= tokens[left];
+                    left++;
+                    score++;
+                    res = Math.Max(score, res);
+                }
+                else if (score > 0)
+                {
+                    power += tokens[right];
+                    score--;
+                    right--;
+
+                }
+                else break;
+            }
+            return res;
+        }
+    }
+}
