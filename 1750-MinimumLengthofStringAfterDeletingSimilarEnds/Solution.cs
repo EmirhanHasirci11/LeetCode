a28ccb8f522061b633689df75110f36e814cba2d
Emirhan Hasırcı <emir_han886@outlook.com>
2024-03-05 07:08:19 +0300

    1750-MinimumLengthofStringAfterDeletingSimilarEnds

---
+++ 1750-MinimumLengthofStringAfterDeletingSimilarEnds/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace MinimumLengthofStringAfterDeletingSimilarEnds
+{
+    internal class Solution
+    {
+        public int MinimumLength(string s)
+        {
+            int i = 0, j = s.Length - 1;
+            while (i < j && s[i] == s[j])
+            {
+                char c = s[j];
+                while (i <= j && s[i] == c)
+                {
+                    ++i;
+                }
+                while (i <= j && s[j] == c)
+                {
+                    --j;
+                }
+            }
+            return j - i + 1;
+        }
+    }
+}
