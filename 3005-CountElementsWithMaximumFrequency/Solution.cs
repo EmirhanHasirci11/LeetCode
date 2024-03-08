f0cec976c357129b6c736e4ab2dc11bba190fb89
Emirhan Hasırcı <emir_han886@outlook.com>
2024-03-08 11:19:26 +0300

    3005-CountElementsWithMaximumFrequency

---
+++ 3005-CountElementsWithMaximumFrequency/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace CountElementsWithMaximumFrequency
+{
+    internal class Solution
+    {        
+        public int MaxFrequencyElements(int[] nums)
+        {
+            int result = 0;
+            int max = int.MinValue;
+
+            Dictionary<int, int> res = new();
+
+            foreach(int i in nums)            
+                if (!res.TryAdd(i, 1))
+                    res[i]++;
+
+            foreach(int val in res.Values)
+                max = Math.Max(max, val);
+
+            foreach (int val in res.Values)
+                result += max == val ? max : 0;
+
+            return result;
+
+            
+        }
+    }
+}
