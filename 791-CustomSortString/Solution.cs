2fdfb8f76cd69af1304cd9474c28c22958da875a
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-11 20:32:25 +0300

    791-CustomSortString

---
+++ 791-CustomSortString/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace CustomSortString
+{
+    internal class Solution
+    {        
+        public string CustomSortString(string order, string s)
+        {
+            int[] count = new int[26];
+            foreach (char c in s)            
+                ++count[c - 'a'];  
+            
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in order)            
+                while (count[c - 'a']-- > 0)                
+                    sb.Append(c);  
+                
+            
+            for (char c = 'a'; c <= 'z'; ++c)            
+                while (count[c - 'a']-- > 0)                
+                    sb.Append(c); 
+                            
+            return sb.ToString();
+        }
+    }
+}
