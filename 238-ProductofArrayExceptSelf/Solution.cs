896ac8d5d10a2d221392267251d67a8a4d0eaf05
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-15 11:04:15 +0300

    238-ProductofArrayExceptSelf

---
+++ 238-ProductofArrayExceptSelf/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace ProductofArrayExceptSelf
+{
+    internal class Solution
+    {        
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int lengthOfNums = nums.Length;
+            int[] res=new int[lengthOfNums];
+
+            int left=1; int right=1;
+            for(int i = 0; i < lengthOfNums; i++)
+            {
+                if(i>0)
+                    left*= nums[i-1];
+                res[i] = left;
+            }
+            for (int i = lengthOfNums - 1; i >= 0; i--)
+            {
+                if (i < lengthOfNums - 1)
+                    right *= nums[i + 1];
+                res[i] *= right;
+            }
+            return res;
+        }
+    }
+}
