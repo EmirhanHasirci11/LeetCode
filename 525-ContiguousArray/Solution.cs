c690b97ecfa5c6ac1096707b922ebd8bc4e5bb6d
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-16 11:30:49 +0300

    525-ContiguousArray

---
+++ 525-ContiguousArray/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace ContiguousArray
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int FindMaxLength(int[] nums)
+        {
+            Dictionary<int, int> dict = new();
+            dict[0] = -1;
+            int zeros = 0, ones = 0, max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0) zeros++; else ones++;
+                int diff = zeros - ones;
+                if (dict.ContainsKey(diff))
+                    max = Math.Max(max, i - dict[diff]);
+                else
+                    dict.Add(diff, i);
+            }
+
+            return max;
+        }
+
+    }
+}
