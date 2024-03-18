0f247e73851af9195a7829760ba5bd467ffb18c3
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-18 18:06:50 +0300

    452-MinimumNumberofArrowstoBurstBalloons

---
+++ 452-MinimumNumberofArrowstoBurstBalloons/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace MinimumNumberofArrowstoBurstBalloons
+{
+    internal class Solution
+    {        
+        public int FindMinArrowShots(int[][] points)
+        {
+            Array.Sort(points, (a, b) => a[1].CompareTo(b[1]));
+
+            int arrows = 1;
+            int prevEnd = points[0][1];
+
+            // Count the number of non-overlapping intervals
+            for (int i = 1; i < points.Length; ++i)
+            {
+                if (points[i][0] > prevEnd)
+                {
+                    arrows++;
+                    prevEnd = points[i][1];
+                }
+            }
+
+            return arrows;
+        }
+    }
+}
