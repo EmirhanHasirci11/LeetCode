47177dc9b29b0bffd3553ca8e6fac3dfaa0ebad5
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-20 11:40:55 +0300

    1669-MergeInBetweenLinkedLists

--- 1669-MergeInBetweenLinkedLists/Solution.cs
+++ 1669-MergeInBetweenLinkedLists/Solution.cs
@@ -4,26 +4,20 @@
     {
         public ListNode MergeInBetween(ListNode list1, int a, int b, ListNode list2)
         {
-            ListNode head = list1;
-            int temp = 0;
-            while (list1 != null && temp != a - 1)
-            {
-                list1 = list1.next;
-                temp++;
-            }
-            temp = a;
-            ListNode next = list1.next;
-            list1.next = list2;
-            while (next != null && temp != b + 1)
-            {
-                next = next.next;
-                temp++;
-            }
+            ListNode left = list1;
+            for (int i = 1; i < a; i++)
+                left = left.next;
+
+            ListNode middle = left;
+            for (int i = a; i <= b; i++)
+                middle = middle.next;
+
+            left.next = list2;
             while (list2.next != null)
                 list2 = list2.next;
 
-            list2.next = next;
-            return head;
+            list2.next = middle.next;
+            return list1;
         }
 
     }
