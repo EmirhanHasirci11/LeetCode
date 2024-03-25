4dc48adfc07a05f6375bc51f19b18742c9270231
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-25 08:47:14 +0300

    442-FindAllDuplicatesinanArray

---
+++ 442-FindAllDuplicatesinanArray/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace FindAllDuplicatesinanArray
+{
+    internal class Solution
+    {
+        
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            if (nums.Length==0) return null;
+            List<int> ans=new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[Math.Abs(nums[i]) - 1] < 0)
+                    ans.Add(Math.Abs(nums[i]));
+                nums[Math.Abs(nums[i]) - 1] = -nums[Math.Abs(nums[i]) - 1];
+            }
+            return ans;
+        }
+    }
+}
