dfecc79bb3f84bc1daf2b661edbff468286c1326
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-29 10:40:00 +0300

    2962-CountSubarraysWhereMaxElementAppearsatLeastKTimes

---
+++ 2962-CountSubarraysWhereMaxElementAppearsatLeastKTimes/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Linq;
+
+namespace CountSubarraysWhereMaxElementAppearsatLeastKTimes
+{
+    internal class Solution
+    {        
+        public long CountSubarrays(int[] nums, int k)
+        {
+            long arrayLength = nums.Length;
+            long maxElement = nums.Max();
+            long leftIndex = 0, rightIndex = 0, maxElementCount = 0, subarrayCount = 0;
+
+            while (rightIndex < arrayLength)
+            {
+                if (nums[rightIndex] == maxElement)
+                {
+                    maxElementCount++;
+                }
+                if (maxElementCount >= k)
+                {
+                    while (maxElementCount >= k)
+                    {
+                        subarrayCount += arrayLength - rightIndex;
+                        if (nums[leftIndex] == maxElement)
+                        {
+                            maxElementCount--;
+                        }
+                        leftIndex++;
+                    }
+                }
+                rightIndex++;
+            }
+            return subarrayCount;
+        }
+    }
+}
