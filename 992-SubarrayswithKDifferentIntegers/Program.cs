1145dc9677b3fd64d7e02eb4d8904bdcc2b2f169
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-30 12:53:47 +0300

    992-SubarrayswithKDifferentIntegers

---
+++ 992-SubarrayswithKDifferentIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SubarrayswithKDifferentIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int SubarraysWithKDistinct(int[] nums, int k)
+        {
+            int res = 0, sz = nums.Length;
+            int[] cnt = new int[sz + 1];
+            for (int l = 0, m = 0, r = 0; r < sz; ++r)
+            {
+                if (++cnt[nums[r]] == 1)
+                    if (--k < 0)
+                    {
+                        cnt[nums[m++]] = 0;
+                        l = m;
+                    }
+                if (k <= 0)
+                {
+                    while (cnt[nums[m]] > 1)
+                        --cnt[nums[m++]];
+                    res += m - l + 1;
+                }
+            }
+            return res;
+        }
+    }
+}
