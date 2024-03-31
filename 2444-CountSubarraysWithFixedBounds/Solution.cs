5a728b7b6e230cad9cc239d3e0ccb3eecb88048c
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-31 13:40:30 +0300

    2444-CountSubarraysWithFixedBounds

---
+++ 2444-CountSubarraysWithFixedBounds/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace CountSubarraysWithFixedBounds
+{
+    internal class Solution
+    {        
+        public long CountSubarrays(int[] nums, int minK, int maxK)
+        {
+            long res = 0;
+            int badIdx = -1, leftIdx = -1, rightIdx = -1;
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (!(minK <= nums[i] && nums[i] <= maxK))
+                {
+                    badIdx = i;
+                }
+
+                if (nums[i] == minK)
+                {
+                    leftIdx = i;
+                }
+
+                if (nums[i] == maxK)
+                {
+                    rightIdx = i;
+                }
+
+                res += Math.Max(0, Math.Min(leftIdx, rightIdx) - badIdx);
+            }
+
+            return res;
+        }
+    }
+}
