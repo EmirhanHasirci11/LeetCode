b28574308a6f0359bed18017b786a8dbe63e1d75
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-01 21:07:21 +0300

    58-LengthofLastWord

--- 58-LengthofLastWord/Solution.cs
+++ 58-LengthofLastWord/Solution.cs
@@ -5,6 +5,7 @@
         public int LengthOfLastWord(string s)
         {
             int count = 0;
+
             for (int i = s.Length - 1; i >= 0; i--)
             {
                 if (Char.IsWhiteSpace(s[i]))
