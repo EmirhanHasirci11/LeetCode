895d3d72ab9b415197443030d16a3cce98445db0
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-05 16:08:48 +0300

    1544-MakeTheStringGreat

---
+++ 1544-MakeTheStringGreat/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace MakeTheStringGreat
+{
+    internal class Solution
+    {        
+        public string MakeGood(string s)
+        {
+            var sb = new StringBuilder();
+            foreach (var c in s)
+                if (sb.Length > 0 && Math.Abs(sb[^1] - c) == 32)
+                    sb.Length--;
+                else
+                    sb.Append(c);
+            return sb.ToString();
+        }
+    }
+}
