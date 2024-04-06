697a50db55fb1bbfb369a6810eda619bbf183ad9
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-06 16:59:10 +0300

    1249-MaximumNestingDepthoftheParentheses

---
+++ 1249-MaximumNestingDepthoftheParentheses/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace MaximumNestingDepthoftheParentheses
+{
+    internal class Solution
+    {        
+        public int MaxDepth(string s)
+        {
+            int count = 0;
+            int maxNum = 0;
+            foreach (char c in s)
+            {
+                if (c == '(')
+                {
+                    count++;
+                    if (maxNum < count)
+                        maxNum = count;
+                }
+                else if (c == ')')
+                {
+                    count--;
+                }
+            }
+            return maxNum;
+        }
+    }
+}
