e98835cba70a7175e56f84add4b4c190ee3bd84a
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-08 14:19:11 +0300

    1700-NumberofStudentsUnabletoEatLunch

---
+++ 1700-NumberofStudentsUnabletoEatLunch/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace NumberofStudentsUnabletoEatLunch
+{
+    internal class Solution
+    {
+        public int CountStudents(int[] students, int[] sandwiches)
+        {
+            int[] res = new int[] { 0, 0 };
+            int k;
+            foreach (int i in students)            
+                res[i]++;
+
+            for (k=0; k < students.Length && res[sandwiches[k]] > 0; k++)
+                res[sandwiches[k]]--;
+
+            return students.Length - k;
+
+
+        }
+    }
+}
