90034c11755d4d274277a7529f05d826a7e5d5b3
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-11 03:36:18 +0300

    402-RemoveKDigits

---
+++ 402-RemoveKDigits/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace RemoveKDigits
+{
+    internal class Solution
+    {
+        public string RemoveKdigits(string num, int k)
+        {
+            if (k == num.Length)
+                return "0";
+
+            StringBuilder ms = new();
+
+            foreach (char c in num)
+            {
+                while (k > 0 && ms.Length > 0 && ms[^1] > c)
+                {
+                    ms.Length--;
+                    k--;
+                }
+                ms.Append(c);
+            }
+
+            ms.Length -= k;
+            string result = ms.ToString().TrimStart('0');
+            return result.Length == 0 ? "0" : result;
+        }
+    }
+}
