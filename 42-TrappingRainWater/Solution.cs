88c2ebefbae1064fda2a92ae75ff0607dc5f5d37
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-12 11:41:31 +0300

    42-TrappingRainWater

---
+++ 42-TrappingRainWater/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace TrappingRainWater
+{
+    internal class Solution
+    {        
+        public int Trap(int[] height)
+        {
+            int left = 0, right = height.Length - 1;
+            int leftMax = height[0], rightMax = height[height.Length - 1];
+            int water = 0;
+            while (left < right)
+            {
+                if (leftMax < rightMax)
+                {
+                    left++;
+                    if (leftMax < height[left])                    
+                        leftMax = height[left];                    
+                    else                    
+                        water += leftMax - height[left];
+                    
+                }
+                else
+                {
+                    right--;
+                    if (rightMax < height[right])                    
+                        rightMax = height[right];                    
+                    else                    
+                        water += rightMax - height[right];
+                    
+                }
+            }
+            return water;
+        }
+    }
+}
