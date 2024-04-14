ce379de1102c386a4f4e4b251b96e69a9e2d8d28
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-14 07:43:38 +0300

    404-SumofLeftLeaves

---
+++ 404-SumofLeftLeaves/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace SumofLeftLeaves
+{
+    internal class Solution
+    {
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            public int SumOfLeftLeaves(TreeNode root)
+            {
+                if (root == null) return 0;
+                int ans = 0;
+                if (root.left != null)
+                {
+                    if (root.left.left == null && root.left.right == null) ans += root.left.val;
+                    else ans += SumOfLeftLeaves(root.left);
+                }
+                ans += SumOfLeftLeaves(root.right);
+
+                return ans;
+            }
+        }
+    }
+}
