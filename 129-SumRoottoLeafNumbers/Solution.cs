ad33d9db2d00a79f5537a870ec000bf81a09d219
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-15 23:03:05 +0300

    129-SumRoottoLeafNumbers

---
+++ 129-SumRoottoLeafNumbers/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace SumRoottoLeafNumbers
+{
+    internal class Solution
+    {
+        public int SumNumbers(TreeNode root)
+        {
+            return Sum(root, 0);
+        }
+        public int Sum(TreeNode n, int s)
+        {
+            if (n == null) return 0;
+            if (n.right == null && n.left == null) return s * 10 + n.val;
+            return Sum(n.left, s * 10 + n.val) + Sum(n.right, s * 10 + n.val);
+        }
+    }
+}
