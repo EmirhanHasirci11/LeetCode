cdc5ffebea2e17006914ace2db29f5ddd207eb11
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-16 15:06:27 +0300

    623-AddOneRowtoTree

---
+++ 623-AddOneRowtoTree/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace AddOneRowtoTree
+{
+    internal class Solution
+    {
+        public TreeNode AddOneRow(TreeNode root, int val, int depth)
+        {
+            if (depth == 0 || depth == 1)
+            {
+                TreeNode newroot = new TreeNode(v);
+                newroot.left = depth == 1 ? root : null;
+                newroot.right = depth == 0 ? root : null;
+                return newroot;
+            }
+            if (root != null && depth >= 2)
+            {
+                root.left = AddOneRow(root.left, val, depth > 2 ? depth - 1 : 1);
+                root.right = AddOneRow(root.right, val, depth > 2 ? depth - 1 : 0);
+            }
+            return root;
+        }
+    }
+}
