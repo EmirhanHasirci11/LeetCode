c8248eb0662edc0dafeac1648f9c531d66f57d67
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-17 22:01:30 +0300

    988-SmallestStringStartingFromLeaf

---
+++ 988-SmallestStringStartingFromLeaf/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace SmallestStringStartingFromLeaf
+{
+    internal class Solution
+    {        
+        public string SmallestFromLeaf(TreeNode root)
+        {
+            string res = "";
+            public string SmallestFromLeaf(TreeNode root)
+            {
+                DFS(root, "");
+                return res;
+            }
+
+            public void DFS(TreeNode root, string currStr)
+            {
+                if (root == null)
+                    return;
+                currStr = (char)(root.val + 'a') + currStr;
+                if (root.left == null && root.right == null)
+                    if (res == "" || string.Compare(res, currStr) > 0)
+                        res = currStr;
+                if (root.left != null)
+                    DFS(root.left, currStr);
+                if (root.right != null)
+                    DFS(root.right, currStr);
+            }
+        }
+    }
+}
