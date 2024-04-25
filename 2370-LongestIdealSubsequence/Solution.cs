afa389fc95a690d66f0e4b74272bca116bc76fe0
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-25 15:23:14 +0300

    2370-LongestIdealSubsequence

---
+++ 2370-LongestIdealSubsequence/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LongestIdealSubsequence
+{
+    internal class Solution
+    {
+        public int LongestIdealString(string s, int k)
+        {
+            int[] dp = new int[26];
+            int curr = 0;
+            int longest = 0;
+            foreach (char c in s)
+            {
+                curr = c - 'a';
+                longest = 1;
+                for (int i = 0; i < 26; i++)
+                {
+                    if (Math.Abs(curr - i) <= k)
+                        longest = Math.Max(longest, 1 + dp[i]);
+                }
+                dp[curr] = Math.Max(dp[curr], longest);
+            }
+            return dp.Max();
+        }
+    }
+}
