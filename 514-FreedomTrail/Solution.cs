c78eb1971c0aca5d0665f269f5dead56a4a9fcb8
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-27 13:25:23 +0300

    514-FreedomTrail

---
+++ 514-FreedomTrail/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace FreedomTrail
+{
+    internal class Solution
+    {        
+        public int FindRotateSteps(string ring, string key)
+        {
+            int n = ring.Length;
+            int m = key.Length;
+            int[,] dp = new int[m + 1, n];
+
+            for (int i = m - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dp[i, j] = int.MaxValue;
+                    for (int k = 0; k < n; k++)
+                    {
+                        if (ring[k] == key[i])
+                        {
+                            int diff = Math.Abs(j - k);
+                            int step = Math.Min(diff, n - diff);
+                            dp[i, j] = Math.Min(dp[i, j], step + dp[i + 1, k]);
+                        }
+                    }
+                }
+            }
+
+            return dp[0, 0] + m;
+        }
+    }
+}
