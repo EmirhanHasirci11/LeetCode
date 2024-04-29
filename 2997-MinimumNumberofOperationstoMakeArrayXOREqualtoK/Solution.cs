2f75f9e1fc76266f6daf23e3888ad66a2c9a7ba8
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-29 19:04:52 +0300

    2997-MinimumNumberofOperationstoMakeArrayXOREqualtoK

---
+++ 2997-MinimumNumberofOperationstoMakeArrayXOREqualtoK/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace MinimumNumberofOperationstoMakeArrayXOREqualtoK
+{
+    internal class Solution
+    {
+        public int MinOperations(int[] nums, int k)
+        {
+            int finalXor = 0;
+            foreach (int i in nums)
+                finalXor ^= i;
+
+            int count = 0;
+            while (k > 0 || finalXor > 0)
+            {
+                if ((k % 2) != (finalXor % 2))
+                    count++;
+                k /= 2;
+                finalXor /= 2;
+            }
+            return count;
+
+        }
+    }
+}
