3f815a8845ec4f83ea0ab6c22fc9dc60928f49cc
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-30 14:02:53 +0300

    1915-NumberofWonderfulSubstrings

---
+++ 1915-NumberofWonderfulSubstrings/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace NumberofWonderfulSubstrings
+{
+    internal class Solution
+    {
+        public long WonderfulSubstrings(string word)
+        {
+            var freq = new long[1024];
+            freq[0] = 1;
+            long result = 0; 
+            var mask = 0;
+            foreach (var ch in word)
+            {
+                mask ^= MaskOf(ch);
+                result += freq[mask];                
+                for (var curr = 'a'; curr <= 'j'; curr++)
+                {
+                    var maskToCheck = mask ^ MaskOf(curr);
+                    result += freq[maskToCheck];
+                }
+
+               
+                freq[mask]++;
+            }
+
+            return result;
+        }
+
+        private int MaskOf(char ch)
+        {           
+            var val = ch - 'a';
+            return 1 << val;
+        }
+    
+    }
+}
