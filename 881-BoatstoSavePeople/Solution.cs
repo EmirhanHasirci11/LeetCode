a4347d505fe1722b7e3b4660f1330cc1b7e047e7
Emirhan Hasirci <emir_han886@outlook.com>
2024-05-04 22:41:33 +0300

    881-BoatstoSavePeople

---
+++ 881-BoatstoSavePeople/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace BoatstoSavePeople
+{
+    internal class Solution
+    {        
+        public int NumRescueBoats(int[] people, int limit)
+        {
+            Array.Sort(people);
+            int left = 0, right = people.Length - 1;
+            int boats = 0;
+            while (left <= right)
+            {
+                if (people[left] + people[right] <= limit)
+                {
+                    left++;
+                }
+                right--;
+                boats++;
+            }
+            return boats;
+        }
+    }
+}
