aee731cc053a09f5ac33e3308c688261b2805bfc
Emirhan Hasirci <emir_han886@outlook.com>
2024-05-07 14:38:58 +0300

    2816-DoubleaNumberRepresentedasaLinkedList

---
+++ 2816-DoubleaNumberRepresentedasaLinkedList/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace DoubleaNumberRepresentedasaLinkedList
+{
+    internal class Solution
+    {
+        public ListNode DoubleIt(ListNode head)
+        {
+            var node = head = new ListNode(0, head);
+            for (; node.next != null; node = node.next)
+            {
+                node.val += node.next.val * 2 / 10;
+                node.next.val = node.next.val * 2 % 10;
+            }
+            return head.val == 0 ? head.next : head;
+        }
+    }
+}
