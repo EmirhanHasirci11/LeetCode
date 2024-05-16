79bd9cf48a9335e236ab43aa3d6757f4ee2e2c6f
Emirhan Hasirci <emir_han886@outlook.com>
2024-05-16 21:29:43 +0300

    2331-EvaluateBooleanBinaryTree

---
+++ 2331-EvaluateBooleanBinaryTree/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace EvaluateBooleanBinaryTree
+{
+    internal class Solution
+    {        
+        public bool EvaluateTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            if (root.val == 0)
+            {
+                return false;
+            }
+            if (root.val == 1)
+            {
+                return true;
+            }
+            bool left = EvaluateTree(root.left);
+            bool right = EvaluateTree(root.right);
+            return root.val == 2 ? left | right : left & right;
+        }
+    }
+}   
