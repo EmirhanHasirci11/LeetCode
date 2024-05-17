138861632216782877af5bb8dd7d18babbb4b527
Emirhan Hasirci <emir_han886@outlook.com>
2024-05-17 10:30:21 +0300

    1325-DeleteLeavesWithaGivenValue

---
+++ 1325-DeleteLeavesWithaGivenValue/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace DeleteLeavesWithaGivenValue
+{
+    internal class Solution
+    {        
+        public TreeNode RemoveLeafNodes(TreeNode root, int target)
+        {
+            if (root.left != null) root.left = RemoveLeafNodes(root.left, target);
+            if (root.right != null) root.right = RemoveLeafNodes(root.right, target);
+            return root.left == root.right && root.val == target ? null : root;
+        }
+    }
+}
