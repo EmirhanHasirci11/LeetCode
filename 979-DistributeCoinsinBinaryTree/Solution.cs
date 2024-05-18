aea89b5d150a7bb6f5d94c6e461d40c718098e0e
Emirhan Hasirci <emir_han886@outlook.com>
2024-05-18 12:19:45 +0300

    979-DistributeCoinsinBinaryTree

---
+++ 979-DistributeCoinsinBinaryTree/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace DistributeCoinsinBinaryTree
+{
+    internal class Solution
+    {
+        private int moves = 0;
+
+        private int Dfs(TreeNode node)
+        {
+            if (node == null) return 0;
+            int leftExcess = Dfs(node.left);
+            int rightExcess = Dfs(node.right);
+            moves += Math.Abs(leftExcess) + Math.Abs(rightExcess);
+            return node.val + leftExcess + rightExcess - 1;
+        }
+
+        public int DistributeCoins(TreeNode root)
+        {
+            Dfs(root);
+            return moves;
+        }
+    }
+}
