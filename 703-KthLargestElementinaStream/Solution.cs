712a1eb95a22712089e2cdc7b501263de86bdafb
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2024-08-12 04:10:49 +0300

    703-KthLargestElementinaStream

---
+++ 703-KthLargestElementinaStream/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace KthLargestElementinaStream
+{
+    public class KthLargest
+    {
+        List<int> ListOfSortedNums;
+        int numOfLatest;
+
+        public KthLargest(int k, int[] nums)
+        {
+            numOfLatest = k;
+            ListOfSortedNums = new(nums);
+            ListOfSortedNums.Sort();
+        }
+
+        public int Add(int val)
+        {
+            
+            ListOfSortedNums.Add(val);
+            ListOfSortedNums.Sort();
+            return ListOfSortedNums[ListOfSortedNums.Count - numOfLatest];
+        }
+    }
+}
