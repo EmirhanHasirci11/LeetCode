b43dbe697a14d38866e31b4fd2c3642911a02eb6
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2024-08-22 06:35:14 +0300

    476-NumberComplement

---
+++ 476-NumberComplement/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace NumberComplement
+{
+    internal class Solution
+    {
+        
+        static public int FindComplement(int num)
+        {
+            int bitLength = (int)Math.Log(num, 2) + 1;
+
+            int mask = (1 << bitLength) - 1;
+
+            return num ^ mask;
+
+        }
+    }
+}
