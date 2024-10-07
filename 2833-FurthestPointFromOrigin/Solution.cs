7f929a41991131e12559684c84eac3c8267bcddb
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2024-10-07 20:19:45 +0300

    2833-FurthestPointFromOrigin

---
+++ 2833-FurthestPointFromOrigin/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace FurthestPointFromOrigin
+{
+    internal class Solution
+    {
+        public int FurthestDistanceFromOrigin(string moves)
+        {
+            int countOfDistance = 0;
+            int emptySlot = 0;
+            foreach (char move in moves)
+            {
+                if (move == '_')
+                    emptySlot++;
+                else
+                    countOfDistance += move == 'R' ? 1 : move == 'L' ? -1 : 0;
+            }
+            return Math.Abs(countOfDistance) + emptySlot;
+            
+
+        }
+    }
+}
