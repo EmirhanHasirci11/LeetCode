633b5f09ce9e0f6eadc7d0f2bd07953e56effb84
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2025-01-01 03:40:42 +0300

    1422-MaximumScoreAfterSplittingaString

---
+++ 1422-MaximumScoreAfterSplittingaString/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace MaximumScoreAfterSplittingaString
+{
+
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxScore("1111"));
+        }
+        
+        static public int MaxScore(string s)
+        {
+            int maxScore = int.MinValue;
+            for (int i = 0; i + 1 < s.Length; i++)
+            {
+                string left=s.Substring(0,i==s.Length?i:i+1);
+                string right=s.Substring(i==s.Length?i:i+1,s.Length-i-1);
+                maxScore = (int)Math.Max(maxScore, left.Where(x=>x=='0').Count()+right.Where(x=>x=='1').Count());
+            }
+            return maxScore;
+        }
+    }
+}
