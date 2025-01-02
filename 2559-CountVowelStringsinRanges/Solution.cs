b063a884e7dcf457c6292dfcddb0bb240bf9a33c
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2025-01-02 03:40:14 +0300

    2559-CountVowelStringsinRanges

---
+++ 2559-CountVowelStringsinRanges/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace CountVowelStringsinRanges
+{
+    internal class Solution
+    {        
+        public int[] VowelStrings(string[] words, int[][] queries)
+        {
+            int[] prefix = new int[words.Length + 1];
+            int[] ans = new int[queries.Length];
+            List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (vowels.Contains(words[i][0]) && vowels.Contains(words[i][words[i].Length - 1]))
+                {
+                    prefix[i + 1] = prefix[i] + 1;
+                    continue;
+                }
+                prefix[i + 1] = prefix[i];
+            }
+
+            for (var x = 0; x < queries.Length; x++)            
+                ans[x] = prefix[queries[x][1] + 1] - prefix[queries[x][0]];
+            
+            return ans;
+        }
+    }
+}
