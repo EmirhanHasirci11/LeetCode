b55dcaddaf3c55d81b20398a07c1f28933b230a3
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2025-01-09 12:23:13 +0300

    2185-CountingWordsWithaGivenPrefix

---
+++ 2185-CountingWordsWithaGivenPrefix/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace CountingWordsWithaGivenPrefix
+{
+    internal class Solution
+
+    {
+        public int PrefixCount(string[] words, string pref)
+        {
+            int countOfPrefixMatch = 0;
+            foreach (var word in words)
+            {
+                if(word.StartsWith(pref, StringComparison.OrdinalIgnoreCase))
+                    countOfPrefixMatch++;
+            }
+            return countOfPrefixMatch;
+        }
+    }
+}
