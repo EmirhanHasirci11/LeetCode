f8d18302e4824c599d56eac45bd809a44aba8892
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2025-01-13 03:28:04 +0300

    3223-MinimumLengthofStringAfterOperations

---
+++ 3223-MinimumLengthofStringAfterOperations/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace MinimumLengthofStringAfterOperations
+{
+    internal class Solution
+    {
+        public int MinimumLength(string s)
+        {
+            var map = new int[26];
+            var n = s.Length;
+            var removed = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int ch = s[i] - 'a';
+                map[ch]++;
+
+                if (map[ch] == 3)
+                {
+                    map[ch] -= 2;
+                    removed += 2;
+                }
+            }
+            return n - removed;
+        }
+    }
+}
