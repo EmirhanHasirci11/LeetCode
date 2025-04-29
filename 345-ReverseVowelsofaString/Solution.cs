150b6d065c64dd64820366490d11f84bbe3ae091
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2025-04-29 11:08:33 +0300

    345-ReverseVowelsofaString

---
+++ 345-ReverseVowelsofaString/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace ReverseVowelsofaString
+{
+    internal class Solution
+    {
+        public string ReverseVowels(string s)
+        {
+            char[] word = s.ToCharArray();
+            int start = 0;
+            int end = s.Length - 1;
+            string vowels = "aeiouAEIOU";
+
+            while (start < end)
+            {
+                while (start < end && !vowels.Contains(word[start].ToString()))
+                {
+                    start++;
+                }
+                while (start < end && !vowels.Contains(word[end].ToString()))
+                {
+                    end--;
+                }
+
+                char temp = word[start];
+                word[start] = word[end];
+                word[end] = temp;
+
+                start++;
+                end--;
+            }
+
+            return new string(word);
+        }
+    }
+}
