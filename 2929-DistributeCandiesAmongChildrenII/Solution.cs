6012a8f970b153ae9daa55d219d9dcc32a42cbcb
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2025-06-01 17:49:32 +0300

    2929-DistributeCandiesAmongChildrenII

---
+++ 2929-DistributeCandiesAmongChildrenII/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace DistributeCandiesAmongChildrenII
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public long DistributeCandies(int n, int limit)
+        {
+            long res =0;
+            for (int i = 0; i <= Math.Min(limit, n); i++)
+            {
+                if (n - i <= 2 * limit)
+                {
+                    res += Math.Min(n - i, limit) - Math.Max(0, n - i - limit) + 1;
+                }
+            }
+            return res;
+        }   
+    }
+}
