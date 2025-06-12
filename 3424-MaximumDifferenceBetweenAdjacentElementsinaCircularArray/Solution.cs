80a4096598e60514264dc510604d4f74d2a8ac1d
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2025-06-12 19:03:43 +0300

    3424-MaximumDifferenceBetweenAdjacentElementsinaCircularArray

---
+++ 3424-MaximumDifferenceBetweenAdjacentElementsinaCircularArray/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace MaximumDifferenceBetweenAdjacentElementsinaCircularArray
+{
+    internal class Solution
+    {
+        public int MaxAdjacentDistance(int[] nums)
+        {
+            int maxDistance = 0;
+            int n = nums.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                int next = (i + 1) % n; // wrap around for circular adjacency
+                int diff = Math.Abs(nums[i] - nums[next]);
+                maxDistance = Math.Max(maxDistance, diff);
+            }
+
+            return maxDistance;
+        }
+    }
+}
